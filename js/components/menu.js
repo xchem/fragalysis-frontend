be5c6f56fe7d7d218f5f60c2cfd30a2cdb0926fd
abradle <you@example.com>
2018-03-22 10:29:20 -0400

    DEV

--- js/components/menu.js
+++ js/components/menu.js
@@ -1,7 +1,7 @@
 /**
  * Created by abradley on 20/03/2018.
  */
-import {slide as Menu} from 'react-burger-menu';
+import {pushRotate as Menu} from 'react-burger-menu';
 import {decorator as reduxBurgerMenu} from 'redux-burger-menu';
 
 export default reduxBurgerMenu(Menu);
