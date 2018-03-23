c766a08ab1a1f3f209c71cb1f1d743e7f171d84e
abradle <you@example.com>
2018-03-23 07:32:39 -0400

    DEV

--- js/components/header.js
+++ js/components/header.js
@@ -3,11 +3,27 @@
  */
 import React from 'react';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap'
+import { Typeahead}
 
 export default class Header extends React.Component {
   render() {
-    return <div>
-      <Navbar/>
-      </div>
+    return <Navbar>
+                <Typeahead
+                    labelKey="name"
+                    options={this.props.target_id_list}
+                    placeholder="Choose a target..."
+                />
+      </Navbar>
   }
 }
+
+function mapStateToProps(state) {
+  return {
+      target_id_list: state.apiReducers.target_id_list,
+      target_on: state.apiReducers.target_on
+  }
+}
+const mapDispatchToProps = {
+    setTargetOn: apiActions.setTargetOn,
+}
+export default connect(mapStateToProps, mapDispatchToProps)(TargetList)
