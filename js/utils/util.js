e98209c9ca0fd82476d0116533305c0821131196
abradle <you@example.com>
2018-03-28 13:03:06 +0100

    DEV

--- js/utils/util.js
+++ js/utils/util.js
@@ -39,6 +39,12 @@ function arraysEqual(a, b) {
     }
     return true;
 }
-
+export function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
 
 export { getQueryStringParameterByName, inspect }
