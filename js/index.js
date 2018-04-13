0cdff863f6cb387fc724c25b9d20b5455cda27bf
abradle <you@example.com>
2018-04-13 16:05:18 +0200

    Refactored and added picker

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import Root from './containers/Root'
+import PickerRoot from './containers/pickerRoot'
 import '../css/index.css';
 
 render(
-  <Root />,
+  <PickerRoot />,
   document.getElementById('app')
 ) 
