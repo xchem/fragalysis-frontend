dcfc30d683b34203e87713670a3ddcc199b3d3ba
abradle <you@example.com>
2018-04-13 16:05:18 +0200

    Refactored and added picker

--- js/containers/tindspectRoot.js
+++ js/containers/tindspectRoot.js
@@ -4,12 +4,12 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import configureStore from '../configureStore'
-import Tindspect from '../components/overallComponents';
+import Tindspect from '../components/tindspectHolder';
 
 
 const store = configureStore()
  
-export default class Root extends Component {
+export default class PickerRoot extends Component {
     render() {
         return (
             <Provider store={store}>
