bc2f9a614573ec203ceff1b23bf6f93f8f0cc115
abradle <you@example.com>
2018-04-17 16:32:49 +0100

    DEV

--- js/components/panddaSiteList.js
+++ js/components/panddaSiteList.js
@@ -20,7 +20,7 @@ class PanddaSiteList extends GenericList {
         this.generateObject = this.generateObject.bind(this);
     }
     render() {
-        return null;
+        return "HELLO MOTO";
     }
 
     generateObject(data,selected=false){
