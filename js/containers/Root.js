b046cd02182f92ab88a39c2e62ce371fa9206926
Anthony Bradley <anthony.richard.bradley@gmail.com>
2018-04-17 20:08:30 +0100

    Revert "Refactored and added picker"
    This reverts commit 0cdff863f6cb387fc724c25b9d20b5455cda27bf.

--- js/containers/Root.js
+++ js/containers/Root.js
@@ -4,12 +4,12 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import configureStore from '../configureStore'
-import Tindspect from '../components/tindspectHolder';
+import Tindspect from '../components/overallComponents';
 
 
 const store = configureStore()
  
-export default class PickerRoot extends Component {
+export default class Root extends Component {
     render() {
         return (
             <Provider store={store}>
