bdc267a1c89d07b652e886468687b411c76da5ba
abradle <you@example.com>
2018-04-19 17:00:57 +0100

    BUGFIX

--- js/components/panddaSlider.js
+++ js/components/panddaSlider.js
@@ -13,16 +13,31 @@ class PanddaSlider extends Slider{
         super(props);
         this.slider_name = "Pandda Site"
     }
+
+
+    newOption(new_value){
+        if(this.props.event_on!=undefined) {
+            for (var index in this.props.event_list) {
+                if (this.props.event_list[index].id == this.props.event_on) {
+                    this.props.deleteObject(Object.assign({display_div: "pandda_major"}, this.generateEventMapObject(this.props.event_list[index])))
+                }
+            }
+            this.props.setEventOn(undefined);
+        }
+    }
 }
 
 function mapStateToProps(state) {
   return {
+      evnet_on: state.apiReducers.pandda_event_on,
+      event_list: state.apiReducers.pandda_event_list,
       object_list: state.apiReducers.pandda_site_list,
       object_on: state.apiReducers.pandda_site_on
   }
 }
 const mapDispatchToProps = {
     setObjectOn: apiActions.setPanddaSiteOn,
+    setEventOn: apiActions
     setObjectList: apiActions.setPanddaEventList
 }
 export default connect(mapStateToProps, mapDispatchToProps)(PanddaSlider);
