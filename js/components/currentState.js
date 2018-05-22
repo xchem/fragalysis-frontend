f089614ce62263e9c43ba46801e48dfae770c8b9
ricgillams <ric.gillams@gmail.com>
2018-05-22 09:35:07 +0100

    debug

--- js/components/currentState.js
+++ js/components/currentState.js
@@ -19,7 +19,9 @@ class SummaryView extends React.Component{
         return <div>
             <Well>
                 <Row>
-                    <h3><b>Current State: /n</b></h3>
+                    <h3><b>Current State: </b></h3>
+                </Row>
+                <Row>
                     <h3>Objects In View: {this.props.object_in_view}</h3>
                 </Row>
             </Well>
