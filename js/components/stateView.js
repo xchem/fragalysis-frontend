4f64ac8e1075c4ad1ef4d84c53560e4a50eb89fb
ricgillams <ric.gillams@gmail.com>
2018-05-22 14:19:01 +0100

    debug

--- js/components/stateView.js
+++ js/components/stateView.js
@@ -22,6 +22,8 @@ class SummaryView extends React.Component{
                 <Col xs={6} md={6}>
                     <h3><b>Current State:</b></h3>
                     <h3>Target on? <b>{this.props.target_on}</b></h3>
+                    <h3>Target id? <b>{this.props.target_id}</b></h3>
+                    <h3>Project id? <b>{this.props.project_id}</b></h3>
                     <Button bsSize="large" bsStyle="success">Display State</Button>
                 </Col>
                 </Row>
@@ -37,7 +39,10 @@ function mapStateToProps(state) {
       vector_list: state.selectionReducers.vector_list,
       querying: state.selectionReducers.querying,
       to_query: state.selectionReducers.to_query,
-      target_on: state.apiReducers.target_on  }
+      target_on: state.apiReducers.target_on,
+      target_id: state.apiReducers.target_id,
+      project_id: state.apiReducers.project_id
+  }
 }
 
 
