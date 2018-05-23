c54bc40b9c93a7a671a6b12a1b731df77546bd01
ricgillams <ric.gillams@gmail.com>
2018-05-23 08:59:15 +0100

    debug

--- js/reducers/stateReducers.js
+++ js/reducers/stateReducers.js
@@ -5,18 +5,25 @@ import * as actions from '../actions/actonTypes'
 
 
 const INITIALSTATE = {
-
+    objectsToLoad: {},
+    objectsInView: {}
 }
 
 
 export default function stateReducers(state = INITIALSTATE, action) {
+    console.log('REDUCERS FIRED OFF. OLD STATE');
     console.log(state);
+    console.log('action.type=' + action.type);
 
     switch (action.type) {
         // Defined in initialState - but may be needed if we want to load a different structure
         case actions.STATE_ALERT:
+            var objectsInView = state.objectsInView
+
             return Object.assign({}, state, {
-                alert("state");
+                objectsInView: objectsInView
+            return Object.assign({}, state, {
+                alert(objectsInView);
             })
 
         case actions.STATE_SPECIFY:
