4cd516029e8f0330714205518b12165e9b145fe5
ricgillams <ric.gillams@gmail.com>
2018-05-24 11:57:05 +0100

    debug

--- js/components/stateView.js
+++ js/components/stateView.js
@@ -20,7 +20,7 @@ class SummaryView extends React.Component{
 
     convert_state_to_template(input_json) {
         var templateOut = {uuid: "UUID", title: "TITLE", scene : "JSON_OF_SCENE"}
-        outputArray.scene = input_json;
+        templateOut.scene = input_json;
         return templateOut;
     }
 
