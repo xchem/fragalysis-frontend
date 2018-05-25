e867f4a11d6e05d2373259980e880e2114f581b2
ricgillams <ric.gillams@gmail.com>
2018-05-25 12:18:38 +0100

    debug

--- js/components/stateView.js
+++ js/components/stateView.js
@@ -61,7 +61,6 @@ class SummaryView extends React.Component{
                     <h3>Objects in view: <b>{this.props.objects_in_view}</b></h3>
                     <h3>Number of objects? <b>{Object.keys(this.props.objects_in_view).length}</b></h3>
                     <h3>Stringified: <b>{JSON.stringify(this.props.objects_in_view)}</b></h3>
-                    <h3>Template: <b>{this.props.objects_in_view}</b></h3>
                     <Button bsSize="large" bsStyle="success" onClick={this.props.stateAlert}>Display State</Button>
                     <Button bsSize="large" bsStyle="success" onClick={this.handleStateImport}>Load State</Button>
                 </Col>
