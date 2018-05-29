e316cc2dec46090b02b003386f6f444850dee011
ricgillams <ric.gillams@gmail.com>
2018-05-29 13:26:50 +0100

    debug

--- js/components/stateView.js
+++ js/components/stateView.js
@@ -43,14 +43,14 @@ class SummaryView extends React.Component{
     
     handleStateState(){
         var stateState = JSON.stringify(this.props.objects_in_view);
-        var TITLE = 'title';
         var UUID = 'uuid';
+        var TITLE = Object.keys(this.props.objects_in_view).length
         var formattedState = {
-            "uuid": UUID,
-            "title": TITLE,
-            "scene": stateState
+            uuid: UUID,
+            title: TITLE,
+            scene: stateState
         };
-        return alert(formattedState)
+        return alert(JSON.stringify(formattedState))
     }
 
         render(){
