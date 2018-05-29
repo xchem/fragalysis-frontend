5d81847478aa31486fea9b97f50752d5959966e2
ricgillams <ric.gillams@gmail.com>
2018-05-29 15:49:29 +0100

    debug

--- js/components/stateView.js
+++ js/components/stateView.js
@@ -9,39 +9,9 @@ import * as stateActions from '../actions/stateActions'
 class SummaryView extends React.Component{
     constructor(props) {
         super(props);
-        this.handleStateImport = this.handleStateImport.bind(this);
-        this.handleExport = this.handleExport.bind(this);
-        this.handleStateExport = this.handleStateExport.bind(this);
         this.handleStateState = this.handleStateState.bind(this);
-//        this.handleDescribeState = this.handleDescribeState.bind(this);
     }
 
-    handleExport() {
-        var content = this.convert_state_to_template(this.props.objects_in_view);
-        var encodedUri = encodeURI(JSON.stringify(content));
-        var link = document.createElement("a");
-        link.setAttribute("href", encodedUri);
-        link.setAttribute("download", "follow_ups.csv");
-        document.body.appendChild(link); // Required for FF
-        link.click();
-   }
-
-    handleStateExport(){
-        var jsonNglState = JSON.stringify(this.props.objects_in_view);
-        var encodedUri = encodeURI(jsonNglState);
-        var link = document.createElement("a");
-        link.setAttribute("href", encodedUri);
-        link.setAttribute("download", "savedState.json");
-        document.body.appendChild(link); // Required for FF
-        link.click();
-    }
-
-    handleStateImport(){
-        var insertedState = prompt("Please insert state here:");
-        return insertedState;
-        this.props.objects_to_load = insertedState;
-    }
-    
     handleStateState(){
         var stateState = JSON.stringify(this.props.objects_in_view);
         const uuidv4 = require('uuid/v4');
@@ -53,11 +23,7 @@ class SummaryView extends React.Component{
         };
         return alert(JSON.stringify(formattedState))
     }
-
-/*    handleDescribeState(){
-        const { handleDescribe } = this.props
-    }
-*/
+    
         render(){
         return <div>
             <Well>
@@ -68,9 +34,8 @@ class SummaryView extends React.Component{
                     <h3>Number of objects? <b>{Object.keys(this.props.objects_in_view).length}</b></h3>
                     <h3>Stringified: <b>{JSON.stringify(this.props.objects_in_view)}</b></h3>
                     <Button bsSize="large" bsStyle="success" onClick={this.handleStateState}>Display State</Button>
-                    <Button bsSize="large" bsStyle="success" onClick={this.handleStateImport}>Load State</Button>
                     <form>
-                        <label> Name: <input type="text" name="insert state here" />
+                        <label> Insert state here: <input type="text" name="name" />
                         </label><input type="submit" value="Submit" />
                     </form>
                 </Col>
