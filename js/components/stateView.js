e301bda9301fc7b723f42b38cc4bcc1f35232701
ricgillams <ric.gillams@gmail.com>
2018-05-30 10:55:59 +0100

    debug

--- js/components/stateView.js
+++ js/components/stateView.js
@@ -33,6 +33,7 @@ class SummaryView extends React.Component{
                     <h3>Target on? <b>{this.props.target_on}</b></h3>
                     <h3>Number of objects? <b>{Object.keys(this.props.objects_in_view).length}</b></h3>
                     <h3>Orientation? <b>{this.props.orientation}</b></h3>
+                    <h3>Water? <b>{this.props.water}</b></h3>
                     <h3>Stringified: <b>{JSON.stringify(this.props.objects_in_view)}</b></h3>
                     <Button bsSize="large" bsStyle="success" onClick={this.handleStateState}>Display State</Button>
                     <form>
@@ -55,6 +56,7 @@ function mapStateToProps(state) {
       to_query: state.selectionReducers.to_query,
       target_on: state.apiReducers.target_on,
       orientation: state.nglReducers.orientation,
+      water: state.nglReducers.water,
       objects_in_view: state.nglReducers.objectsInView,
       objects_to_load: state.nglReducers.objectsToLoad
   }
