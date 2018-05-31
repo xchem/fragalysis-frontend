e91108db0f6b236bdb1ac0023c1e0561d2189e76
ricgillams <ric.gillams@gmail.com>
2018-05-31 16:36:45 +0100

    debug

--- js/components/stateView.js
+++ js/components/stateView.js
@@ -28,7 +28,7 @@ class SummaryView extends React.Component{
     }
 
     handleStateOrientation() {
-        var stateOrientation = this.props.nglOrientation
+        var stateOrientation = this.props.ngl_orientation
         return alert(JSON.stringify(stateOrientation))
     }
 
@@ -40,7 +40,7 @@ class SummaryView extends React.Component{
                     <h3><b>Current State:</b></h3>
                     <h3>Target on? <b>{this.props.target_on}</b></h3>
                     <h3>Number of objects? <b>{Object.keys(this.props.objects_in_view).length}</b></h3>
-                    <h3>Orientation? <b>{JSON.stringify(this.props.nglOrientation)}</b></h3>
+                    <h3>Orientation? <b>{JSON.stringify(this.props.ngl_orientation)}</b></h3>
                     <h3>Stringified: <b>{JSON.stringify(this.props.objects_in_view)}</b></h3>
                     <Button bsSize="large" bsStyle="success" onClick={this.handleStateState}>Display State</Button>
                     <Button bsSize="large" bsStyle="success" onClick={this.handleStateOrientation}>Display Orientation</Button>
@@ -67,7 +67,7 @@ function mapStateToProps(state) {
       color: state.nglReducers.color,
       objects_in_view: state.nglReducers.objectsInView,
       objects_to_load: state.nglReducers.objectsToLoad
-      nglOrientation: state.nglReducers.nglOrientation
+      ngl_orientation: state.nglReducers.nglOrientation
   }
 }
 
