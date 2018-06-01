7b8a1299f0cd6b4bf826b6aab29c0957b5392091
ricgillams <ric.gillams@gmail.com>
2018-06-01 12:24:04 +0100

    debug

--- js/components/stateView.js
+++ js/components/stateView.js
@@ -43,9 +43,8 @@ class SummaryView extends React.Component{
                     <h3>Target on? <b>{this.props.target_on}</b></h3>
                     <h3>Number of objects? <b>{Object.keys(this.props.objects_in_view).length}</b></h3>
                     <h3>Orientation? <b>{JSON.stringify(this.props.ngl_orientation)}</b></h3>
-                    <h3>Stringified: <b>{JSON.stringify(this.props.objects_in_view)}</b></h3>
+                    <h3>Stringified state: <b>{JSON.stringify(this.props.objects_in_view)}</b></h3>
                     <Button bsSize="large" bsStyle="success" onClick={this.handleStateState}>Display State</Button>
-                    <Button bsSize="large" bsStyle="success" onClick={this.handleStateOrientation}>Display Orientation</Button>
                     <form>
                         <label> Insert state here: <input type="text" name="name" />
                         </label><input type="submit" value="Submit" />
