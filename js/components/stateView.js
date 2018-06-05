b4c54b83d30be011bc6966b6bef82bd4ef317705
ricgillams <ric.gillams@gmail.com>
2018-06-05 10:29:00 +0100

    debug

--- js/components/stateView.js
+++ js/components/stateView.js
@@ -67,7 +67,7 @@ class StateView extends React.Component{
         }).then(function(response) {
             return response.json();
         }).then(function(myJson) {
-            console.log(myJson);
+            alert(window.location + "/api/viewscene/" + myJson.id.toString())
         });
     }
 
