e38e59c8b2bfd57126debdb723790eb3323884c3
ricgillams <ric.gillams@gmail.com>
2018-06-05 17:06:16 +0100

    debug

--- js/components/stateView.js
+++ js/components/stateView.js
@@ -21,8 +21,8 @@ class StateView extends React.Component{
     }
 
     handleGetOrientation() {
-        this.setState(prevState => ({this.props.orientation_requested: !prevState.this.props.orientation_requested}))
-        var orientation_requested = JSON.stringify(this.props.orientation_requested);
+        var orientation_requested = this.props.orientation_requested;
+        this.setState(prevState => ({orientation_requested: !prevState.orientation_requested}))
         var orientation_acquired = JSON.stringify(this.props.orientation_acquired);
         var orientationStates = {
             requestOrientation: orientation_requested,
