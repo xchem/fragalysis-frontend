2d4d43c1a4694d35430818753dad660d485ee2f3
ricgillams <ric.gillams@gmail.com>
2018-06-12 09:43:05 +0100

    Debug

--- js/components/nglComponents.js
+++ js/components/nglComponents.js
@@ -338,6 +338,11 @@ export class NGLView extends React.Component {
     }
 
     setNglOrientation(orientationToSet) {
+        if (orientationToSet == undefined) {
+            alert(orientationToSet);
+            return
+        }
+        alert(orientationToSet);
         var curr_orient = this.stage.viewerControls.getOrientation();
         for (var i = 0; i < curr_orient.elements.length; i += 1) {
             curr_orient.elements[i] = orientationToSet[i];
