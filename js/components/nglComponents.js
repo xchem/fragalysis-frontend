ca2001423beb85bd5ee02c195b4d8af56cff637e
ricgillams <ric.gillams@gmail.com>
2018-06-12 09:17:45 +0100

    Debug

--- js/components/nglComponents.js
+++ js/components/nglComponents.js
@@ -340,10 +340,10 @@ export class NGLView extends React.Component {
     setNglOrientation(orientationToSet) {
         if (this.props.orientationToSetFlag === true) {
             var exampleOrientation = [-62.2, 39.9, 30.4, 0, 24.5, 66.5, -36.9, 0, -43.8, -19.4, -64.0, 0, 0, 0, 0, 1];
-            for (var i = 0, l = exampleOrientation.length; i < l; i++) {
+/*            for (var i = 0, l = exampleOrientation.length; i < l; i++) {
                 exampleOrientation[i] = orientationToSet[i];
             }
-            this.stage.viewerControls.orient(exampleOrientation);
+*/            this.stage.viewerControls.orient(exampleOrientation);
 //            this.stage.viewerControls.orient(JSON.parse("[" + orientationToSet + "]"));
 //            this.stage.viewerControls.orient(orientationToSet);
 //            this.stage.viewerControls.orient([-62.2,39.9,30.4,0,24.5,66.5,-36.9,0,-43.8,-19.4,-64.0,0,0,0,0,1]);}
