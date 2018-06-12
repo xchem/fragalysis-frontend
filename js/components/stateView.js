4835494f51d6e9413fa32c244cb6406e3d5f33d4
ricgillams <ric.gillams@gmail.com>
2018-06-12 08:01:40 +0100

    Debug

--- js/components/stateView.js
+++ js/components/stateView.js
@@ -76,8 +76,8 @@ class StateView extends React.Component{
     handleOrientationJson(myJson){
         var myPreDict = JSON.parse(myJson.scene);
         var orientationToSet = JSON.parse(myPreDict.orientation);
-//        this.props.requestToSetOrientation();
-        alert(orientationToSet);
+        this.props.requestToSetOrientation();
+//        alert(orientationToSet);
     }
 
     handleRenderOrientation(){
