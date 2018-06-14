053599320e86ec560ee0aa594dc3dde2cc6b03a6
Ric Gillams <ric.gillams@gmail.com>
2018-06-14 16:51:17 +0100

    modal debug

--- js/components/modalLoadingScreen.js
+++ js/components/modalLoadingScreen.js
@@ -19,10 +19,10 @@ export class ModalLoadingScreen extends React.Component {
     }
 
     render() {
-        // var loadingState = this.props.loadingState;
-        // if (!loadingState) {
-        //     return null;
-        // }
+        var loadingState = this.props.loadingState;
+        if (!loadingState) {
+            return null;
+        }
         return (
             <div>
                 <ReactModal isOpen={this.props.loadingState} onRequestClose={this.setLoadingStateFalse()}>
