6c8758fbc53a82dcf2845b8c1d8eb37e93214b43
Ric Gillams <ric.gillams@gmail.com>
2018-06-15 18:13:36 +0100

    logo debug

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
       { test: /\.js$/, enforce: "pre", loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, enforce: "pre",  loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.png$/, loader: "file-loader" },
+      { test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/, loader: 'url-loader?limit=100000' },
     ]
   },
 
