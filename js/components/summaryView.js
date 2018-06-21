88c1c43e22df4ca2e3a949d5c512c72ace274915
abradle <you@example.com>
2018-06-21 14:14:24 +0100

    Attempt at using await

--- js/components/summaryView.js
+++ js/components/summaryView.js
@@ -146,31 +146,45 @@ class SummaryView extends React.Component{
         return output_dict;
     }
 
+    async getFetch(url){
+        const response = fetch(url);
+        const output_json = await response.json();
+        return output_json;
+    }
+
     handleDocking() {
         // Get the APO protein
         this.props.to_query_prot_id;
+        // This should be defined by type
+        var url = window.location.protocol + "//" + window.location.host
+            + "/api/protpdb/" + this.props.to_query_prot_id.toString() + "/";
+        const pdb_data = this.getFetch(url)["pdb_data"];
         // Get the Original molecule
         const orig_mol = this.props.to_query_sdf_info;
         // Get the elaborations and the vector(s)
         var to_buy_by_vect = this.getToBuyByVect(this.props.to_buy_list);
-        for(var constraint in to_buy_by_vect){}
+        var counter = 0;
+        var zip = new JSZip();
+        for(var constraint in to_buy_by_vect) {
+            var folder = zip.folder(constraint)
             // Get the docking script
             const docking_script = "/usr/bin/obabel -imol /data/reference.sdf -h -O /data/reference_hydrogens.sdf\n" +
-            "/usr/bin/obabel -ismi /data/input.smi -h --gen3D -O /data/input_hydrogens.sdf\n" +
-            "/usr/bin/obabel -ipdb /data/receptor.pdb -O /data/receptor.mol2\n" +
-            '/rDock_2013.1_src/bin/sdtether /data/reference_hydrogens.sdf  /data/input_hydrogens.sdf /data/output.sdf "'+constraint+'"\n' +
-            "/rDock_2013.1_src/bin/rbcavity -was -d -r /data/recep.prm\n" +
-            "/rDock_2013.1_src/bin/rbdock -i /data/output.sdf -o /data/docked.sdf -r /data/recep.prm -p dock.prm -n 9"
+                "/usr/bin/obabel -ismi /data/input.smi -h --gen3D -O /data/input_hydrogens.sdf\n" +
+                "/usr/bin/obabel -ipdb /data/receptor.pdb -O /data/receptor.mol2\n" +
+                '/rDock_2013.1_src/bin/sdtether /data/reference_hydrogens.sdf  /data/input_hydrogens.sdf /data/output.sdf "' + constraint + '"\n' +
+                "/rDock_2013.1_src/bin/rbcavity -was -d -r /data/recep.prm\n" +
+                "/rDock_2013.1_src/bin/rbdock -i /data/output.sdf -o /data/docked.sdf -r /data/recep.prm -p dock.prm -n 9"
             // Save as a zip
-            var zip = new JSZip();
-            zip.file("run.sh", docking_script);
-            zip.file("input.smi", this.generate_smiles(this.props.to_buy_list));
-            zip.file("receptor.pdb", "DUMMY.pdb");
-            zip.file("reference.sdf", orig_mol);
-            zip.generateAsync({type:"blob"})
-                .then(function(content) {
-                    FileSaver.saveAs(content, "docking.zip");
-                });
+            folder.file("run.sh", docking_script);
+            folder.file("input.smi", this.generate_smiles(this.props.to_buy_list));
+            folder.file("receptor.pdb", pdb_data);
+            folder.file("reference.sdf", orig_mol);
+        }
+        zip.generateAsync({type: "blob"})
+            .then(function (content) {
+                    FileSaver.saveAs(content, "docking_vector" + counter.toString() + ".zip");
+            });
+
     }
 
     getNum() {
