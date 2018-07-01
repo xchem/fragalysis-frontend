c7db85ed181d55dd4928f5ea37afc7f367cf3217
Ric Gillams <ric.gillams@gmail.com>
2018-07-02 00:00:19 +0100

    hotspot visualisation development

--- js/components/hotspotList.js
+++ js/components/hotspotList.js
@@ -32,10 +32,11 @@ class HotspotList extends GenericList {
     }
 
     render_method(data) {
-        var buttonLabel = 'fragment ' + data.prot_id.toString() + ' - ' + data.map_type.toString() + ' hotspot'
-        return <ListGroupItem key={data.id} >
-            <Button value={data} onClick={() => this.loadHotspot(data)}> {buttonLabel} </Button>
-            <Button value={data} onClick={() => this.removeHotspot(data)}> {buttonLabel} off</Button>
+        const doData = data.filter(word => word.map_type.toString() === 'DO');
+        var buttonLabel = 'fragment ' + doData.prot_id.toString() + ' - ' + doData.map_type.toString() + ' hotspot'
+        return <ListGroupItem key={doData.id} >
+            <Button value={doData} onClick={() => this.loadHotspot(doData)}> {buttonLabel} </Button>
+            <Button value={doData} onClick={() => this.removeHotspot(doData)}> {buttonLabel} off</Button>
         </ListGroupItem>
     }
 
