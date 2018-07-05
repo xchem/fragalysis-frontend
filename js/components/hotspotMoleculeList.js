3f01430227c049eb0005a518d10a084f6a69d684
Ric Gillams <ric.gillams@gmail.com>
2018-07-05 19:02:10 +0100

    hotspot visualisation debug

--- js/components/hotspotMoleculeList.js
+++ js/components/hotspotMoleculeList.js
@@ -50,7 +50,7 @@ class HotspotMoleculeList extends GenericList {
             console.log(this.props.message)
             return <Well><Row style={molStyle}>
                 {
-                    this.props.object_list.map((data) => <MoleculeView height={125} width={250} key={data.id} data={data}/>)
+                    this.props.object_list.map((data) => <HotspotMoleculeView height={125} width={250} key={data.id} data={data}/>)
                     this.render_method(data)
                 }
             </Row></Well>;
