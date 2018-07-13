c3798166f5ff9d7121a94c07d054bb892591da19
Ric Gillams <ric.gillams@gmail.com>
2018-07-13 08:52:41 +0100

    hotspot visualisation debug

--- js/components/hotspotView.js
+++ js/components/hotspotView.js
@@ -229,30 +229,30 @@ class HotspotView extends GenericView {
                 <div style={this.current_style}>{svg_image}</div>
             </Col>
             <Col xs={9} md={9}>
-                <Row>
+                {/*<Row>*/}
                 <Toggle onClick={this.onComplex}
                         on={<p>Donor ON</p>}
                         off={<p>Donor OFF</p>}
                         size="xs"
                         offstyle="danger"
                         active={this.state.complexOn}/>
-                </Row>
-                <Row>
+                {/*</Row>*/}
+                {/*<Row>*/}
                     <Toggle onClick={this.onComplex}
                         on={<p>Acceptor ON</p>}
                         off={<p>Acceptor OFF</p>}
                         size="xs"
                         offstyle="danger"
                         active={this.state.complexOn}/>
-                </Row>
-                <Row>
+                {/*</Row>*/}
+                {/*<Row>*/}
                     <Toggle onClick={this.onVector}
                             on={<p>Apolar ON</p>}
                             off={<p>Apolar OFF</p>}
                             size="xs"
                             offstyle="danger"
                             active={this.props.to_query==this.props.data.smiles}/>
-                </Row>
+                {/*</Row>*/}
             </Col>
         </div>
     }
