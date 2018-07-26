1e888429206a8a6cccc7e2de186a55de5665b2f2
Ric Gillams <ric.gillams@gmail.com>
2018-07-26 16:06:29 +0100

    refactor hotspot buttons

--- js/components/hotspotView.js
+++ js/components/hotspotView.js
@@ -117,7 +117,7 @@ class HotspotView extends React.Component {
         var donor = {...this.state.hs_dict.donor};
         donor.Tepid = true;
         this.setState({donor});
-        this.setState(prevState => ({...this.state.hs_dict.donor.Tepid: !prevState.hs_dict.donor.Tepid}))
+        // this.setState(prevState => ({...this.state.hs_dict.donor.Tepid: !prevState.hs_dict.donor.Tepid}))
         const load_var = this.state.hs_dict.donor.Tepid ? "unload" : "load";
         this.fetchHotspotUrl(type, this.props.data.prot_id, load_var, this.hsDict[strength].contour, this.hsDict[strength].opacity)
     }
