886a806d66df560b4af13ba4727d32294950d0b6
Ric Gillams <ric.gillams@gmail.com>
2018-07-30 14:36:25 +0100

    refactor hotspot buttons

--- js/components/hotspotView.js
+++ js/components/hotspotView.js
@@ -114,7 +114,7 @@ class HotspotView extends React.Component {
             for (var strength in this.state.hsDict[type]) {
                 var button = React.createElement(Toggle,
                     { key: strength+type, onClick: function onClick() {
-                        return {_this.onHotspot(strength, type)};
+                        return _this.onHotspot(strength, type);
                         }, on: React.createElement(
                             'p',
                             null,
