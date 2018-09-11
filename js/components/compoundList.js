f36b942822a2504cdf703c7e1d722aabad9c4f2a
Ric Gillams <ric.gillams@gmail.com>
2018-09-11 10:54:26 +0100

    move compound view from summary to compound list

--- js/components/compoundList.js
+++ js/components/compoundList.js
@@ -103,9 +103,9 @@ class CompoundList extends React.Component {
     }
 
     componentWillReceiveProps( { keydown } ){
+        console.log("should set highlighted");
         if ( Object.keys(this.props.highlightedCompound).length === 0 && this.props.this_vector_list != undefined ) {
             if (Object.keys(this.props.this_vector_list).length > 0) {
-                console.log("should set highlighted")
                 this.props.setHighlighted({index: 0, smiles: this.props.this_vector_list[Object.keys(this.props.this_vector_list)][0]})
             }
         }
