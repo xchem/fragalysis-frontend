291743db80ec34c885f0428ac040ea8a24dc2d38
abradle <you@example.com>
2018-09-18 09:51:24 +0100

    Add link

--- js/components/header.js
+++ js/components/header.js
@@ -9,9 +9,8 @@ import * as apiActions from "../actions/apiActions";
 import * as nglActions from "../actions/nglLoadActions";
 import {connect} from "react-redux";
 import * as nglObjectTypes from "../components/nglObjectTypes";
-import {withRouter} from "react-router-dom";
+import {withRouter, Link} from "react-router-dom";
 import TargetList from "./targetList";
-
 
 class Header extends React.Component {
 
