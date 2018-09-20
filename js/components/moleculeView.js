58ddb6601b79cee92f75f4eb371dce0b3ce4f74b
Anthony Bradley <anthony.richard.bradley@gmail.com>
2018-09-20 19:52:18 +0100

    Update molecule view (#75)
    * Update molecule view
    
    * Update
    
    * xs not xsm
    
    * Update mol view
    
    * Update to inline block
    
    * Change to justified buttons
    
    * Revert to vertical
    
    * Update mol view

--- js/components/moleculeView.js
+++ js/components/moleculeView.js
@@ -171,28 +171,23 @@ class MoleculeView extends GenericView {
 
     render() {
         const svg_image = <SVGInline svg={this.state.img_data}/>;
-        const selected_style = {width: this.props.width.toString+'px',
-            height: this.props.height.toString()+'px', backgroundColor: this.colourToggle}
-        this.current_style = this.state.isToggleOn || this.state.complexOn ? selected_style : this.not_selected_style;
-        return <Row>
-            <Well>
-                    <Col xs={0} md={0}></Col>
-                    <Col xs={6} md={6}>
-                        <div style={this.current_style}>{svg_image}</div>
-                    </Col>
-                    <Col xs={6} md={6}>
-                        <ButtonToolbar>
-                            <ToggleButtonGroup vertical type="checkbox" value={this.state.value} onChange={this.handleChange}>
-                                <ToggleButton bsSize="sm" bsStyle="info" value={2}>Ligand</ToggleButton>
-                                <ToggleButton bsSize="sm" bsStyle="info" value={1}>Complex</ToggleButton>
-                                <ToggleButton bsSize="sm" bsStyle="info" value={3}>Vectors</ToggleButton>
-                            </ToggleButtonGroup>
-                        </ButtonToolbar>
-                        <Label bsStyle="default">{this.props.data.protein_code}</Label>
-                    </Col>
-                <Col xs={0} md={0}></Col>
-            </Well>
-        </Row>
+        // Here add the logic that updates this based on the information
+        const refinement = <Label bsStyle="success">{"Refined"}</Label>;
+        const selected_style = {height: this.props.height.toString()+'px', backgroundColor: this.colourToggle}
+        const not_selected_style = {height: this.props.height.toString()+'px'}
+        this.current_style = this.state.isToggleOn || this.state.complexOn ? selected_style : not_selected_style;
+        return <div style={{border: "solid", display: "inline-block"}}>
+                <div style={this.current_style}>{svg_image}</div>
+            <Label bsStyle="default">{this.props.data.protein_code}</Label>
+            {refinement}
+                <ButtonToolbar>
+                    <ToggleButtonGroup vertical block type="checkbox" value={this.state.value} onChange={this.handleChange}>
+                        <ToggleButton bsSize="sm" bsStyle="info" value={2}>Ligand</ToggleButton>
+                        <ToggleButton bsSize="sm" bsStyle="info" value={1}>Complex</ToggleButton>
+                        <ToggleButton bsSize="sm" bsStyle="info" value={3}>Vectors</ToggleButton>
+                    </ToggleButtonGroup>
+                </ButtonToolbar>
+            </div>
     }
 
     getRandomColor() {
