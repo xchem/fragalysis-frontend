5482cd3fcaaaa91d0a75a4646ee9d0faf3d3ec26
abradle <you@example.com>
2018-09-22 08:55:44 +0100

    Add user info

--- js/index.js
+++ js/index.js
@@ -5,6 +5,13 @@ import Root from "./containers/root";
 // Setup log rocket logging
 import LogRocket from 'logrocket';
 LogRocket.init('afxrm2/fragalysis');
+// This is an example script - don't forget to change it!
+LogRocket.identify(DJANGO_CONTEXT["username"], {
+    pk: DJANGO_CONTEXT["pk"],
+    name: DJANGO_CONTEXT["name"],
+    email: DJANGO_CONTEXT["email"]
+});
+
 
 render(
       <Root />, document.getElementById('app')
