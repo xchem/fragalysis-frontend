06086b8616f76e2c10ef3ac3bce5a796619ff4ba
abradle <you@example.com>
2018-09-22 08:22:54 +0100

    Added logrocket

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@ import "babel-polyfill";
 import React from "react";
 import {render} from "react-dom";
 import Root from "./containers/root";
-    
+// Setup log rocket logging
+import LogRocket from 'logrocket';
+LogRocket.init('afxrm2/fragalysis');
+
 render(
       <Root />, document.getElementById('app')
 ) 
