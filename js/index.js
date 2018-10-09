c5aa32c5c70af46430443c1e93baa2312ed9a1f3
Richard Gillams <37295433+ricgillams@users.noreply.github.com>
2018-10-09 11:15:11 +0100

    merge xchem changes (#31)
    * Update sentry and add reporting button (#88)
    * Update to only show dialog when
    a) whole page has gone
    b) user requests to submit feedback

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import {render} from "react-dom";
 import Root from "./containers/root";
 // Sentry logging
-import { init } from '@sentry/browser';
+import { init, showReportDialog } from '@sentry/browser';
 // Setup log rocket logging
 import LogRocket from 'logrocket';
 LogRocket.init('afxrm2/fragalysis');
@@ -15,6 +15,14 @@ LogRocket.identify(DJANGO_CONTEXT["username"], {
 });
 init({
   dsn: 'https://65029677c9dd4a8a94b778cd221efb3d@sentry.io/1286780',
+    beforeSend: (event) => {
+        // Check if it is a particular type of exception -> Show report dialog
+        // E.g. we might not want it show for all exceptions - just custom user ones
+      if(event.exception && event.exception.values[0].value.startsWith("Custom user error.")){
+          showReportDialog()
+      }
+      return event;
+    }
 });
 
 render(
