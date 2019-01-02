5de463f670422f09e329aa74e4fb4f746c871421
Ric Gillams <ric.gillams@gmail.com>
2019-01-02 18:41:37 +0000

    modify session list display for much larger limit

--- js/components/targetList.js
+++ js/components/targetList.js
@@ -30,8 +30,8 @@ class TargetList extends GenericList {
             return <ListGroupItem key={data.id}>
                 <Row>
                     <Col xs={0} md={1} lg={1}></Col>
-                    <Col xs={6} md={6} lg={7}><Row></Row><p></p><Row><p><Link to={preview}>{data.title}</Link></p></Row></Col>
-                    <Col xs={6} md={5} lg={4}><Row></Row><p></p><Row><p><a href={sgcUrl} target="new" styles={{float: 'right'}}>Open SGC summary</a></p></Row></Col>
+                    <Col xs={5} md={5} lg={6}><Row></Row><p></p><Row><p><Link to={preview}>{data.title}</Link></p></Row></Col>
+                    <Col xs={7} md={6} lg={5}><Row></Row><p></p><Row><p><a href={sgcUrl} target="new" styles={{float: 'right'}}>Open SGC summary</a></p></Row></Col>
                 </Row>
             </ListGroupItem>
         } else {
