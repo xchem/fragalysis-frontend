76fe5bcfdf06591855d007d0ea11745f64c538a5
Ric Gillams <ric.gillams@gmail.com>
2019-01-14 14:21:40 +0000

    update sgc link name

--- js/components/targetList.js
+++ js/components/targetList.js
@@ -30,7 +30,7 @@ class TargetList extends GenericList {
             return <ListGroupItem key={data.id}>
                 <Row>
                     <Col xs={5} sm={5} mdOffset={1} md={5} lg={6}><Row></Row><p></p><Row><p><Link to={preview}>{data.title}</Link></p></Row></Col>
-                    <Col xs={7} sm={7} md={6} lg={5}><Row></Row><p></p><Row><p><a href={sgcUrl} target="new" styles={{float: 'right'}}>Open SGC summary</a></p></Row></Col>
+                    <Col xs={7} sm={7} md={6} lg={5}><Row></Row><p></p><Row><p><a href={sgcUrl} target="new" styles={{float: 'right'}}>Model quality overview</a></p></Row></Col>
                 </Row>
             </ListGroupItem>
         } else {
