0b8ea5e6458ee4b015cb298af5c798313504dc5c
Ric Gillams <ric.gillams@gmail.com>
2019-02-07 15:45:41 +0000

    new layout

--- js/components/modalTargetUnrecognised.js
+++ js/components/modalTargetUnrecognised.js
@@ -21,7 +21,7 @@ const customStyles = {
         right: 'auto',
         bottom: '0%',
         marginRight: '-20%',
-        transform: 'translate(-50%, 0%)',
+        transform: 'translate(-50%, 10%)',
         border: '10px solid #7a7a7a'
     }
 };
