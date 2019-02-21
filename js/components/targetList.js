9d3fb9edad2a813c24febdc0d0f9caa14eca6f41
Ric Gillams <ric.gillams@gmail.com>
2019-02-21 10:32:09 +0000

    back to electron density in fragalysis

--- js/components/targetList.js
+++ js/components/targetList.js
@@ -56,7 +56,7 @@ class TargetList extends GenericList {
         }
         var preview = "/viewer/react/preview/target/" + data.title;
         var sgcUrl = "https://thesgc.org/sites/default/files/XChem/"+data.title+"/html/index.html";
-        var sgcUploaded = ['BRD1A', 'DCLRE1AA', 'FALZA', 'FAM83BA', 'HAO1A', 'NUDT4A', 'NUDT5A', 'NUDT7A', 'PARP14A'];
+        var sgcUploaded = ['BRD1A', 'DCLRE1AA', 'FALZA', 'FAM83BA', 'HAO1A', 'NUDT4A', 'NUDT5A', 'NUDT7A', 'NUDT21A', 'PARP14A'];
         if (this.props.openTargetIdList.includes(data.title) === false) {
             if (sgcUploaded.includes(data.title)) {
                 return <ListGroupItem key={data.id}>
