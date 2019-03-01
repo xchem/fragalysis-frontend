e09d45c49c0f638a93e9375358de74451ab3e415
Ric Gillams <ric.gillams@gmail.com>
2019-03-01 17:33:43 +0000

    event map in modal

--- js/components/landing.js
+++ js/components/landing.js
@@ -7,12 +7,22 @@ import {connect} from "react-redux";
 import TargetList from "./targetList";
 import SessionList from "./sessionList";
 import {BrowserBomb} from "./browserBombModal";
+import { Redirect } from 'react-router-dom';
 
 export class Welcome extends React.Component {
     constructor(props) {
         super(props);
+        this.handleFragspectLaunch = this.handleFragspectLaunch.bind(this);
     }
 
+    handleFragspectLaunch(e) {
+        if (e.keyCode === 13) {
+            var target = e.target.value;
+            var fragspectAddress = "/viewer/react/fragspect/target/" + e.target.value;
+            console.log('launch fragspect' + target);
+            return <Redirect to={fragspectAddress}/>
+        }
+    }
 
     render() {
         var text_div;
@@ -31,6 +41,15 @@ export class Welcome extends React.Component {
                         <h1>Welcome to Fragalysis{"\n"}</h1>
                         {text_div}
                     </Row>
+                    <Row style={{height: window.innerHeight * 0.1.toString() + "px"}}></Row>
+                    <Row>
+                        <h2>Fragspect launcher{"\n"}</h2>
+                        <h3>Insert target name and press enter to launch Fragspect*:</h3>
+                        <input id="fragspect_launch" key="fragspectLauncher" style={{width: 250}} defaultValue="insert target name here" onKeyDown={this.handleFragspectLaunch}></input>
+                        <p>*this is only available to users who are registered for the visit in question.</p>
+                        <p>Please ensure you are logged in above and contact a beamline scientist, with the details of the target and visit, if you believe you should be included on a visit.</p>
+                    </Row>
+                    <Row style={{height: window.innerHeight * 0.1.toString() + "px"}}></Row>
                     <Row>
                         <p><a className="inline" href="http://cs04r-sc-vserv-137.diamond.ac.uk:8089/overview/targets/">Target status overview</a> (only accessible within Diamond)</p>
                     </Row>
