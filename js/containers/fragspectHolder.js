211aff7804b7e2fa92c0fbb792119deb7e5477df
Ric Gillams <ric.gillams@gmail.com>
2019-03-04 15:19:09 +0000

    landing launcher for fragspect

--- js/containers/fragspectHolder.js
+++ js/containers/fragspectHolder.js
@@ -4,12 +4,41 @@ import {Row, Col} from "react-bootstrap";
 import FragspectList from "../components/fragspectList";
 import ModalFragspectEventView from "../components/modalFragspectEventView";
 // import ModalStateSave from "../components/modalStateSave";
-
+import {withRouter} from "react-router-dom";
+import * as apiActions from "../actions/apiActions";
 
 class Fragspect extends Component {
 
     constructor(props) {
         super(props)
+    }
+
+    updateTarget(){
+        var target = this.props.match.params.target;
+        // Get from the REST API
+        // if (this.props.targetIdList.length != 0) {
+        //     var targetUnrecognised = true;
+        //     for (var i in this.props.targetIdList) {
+        //         if (target == this.props.targetIdList[i].title) {
+        //             targetUnrecognised = false;
+        //         }
+        //     }
+        // }
+        // this.props.setTargetUnrecognised(targetUnrecognised);
+        fetch(window.location.protocol + "//" + window.location.host+"/xcdb/fragspect/?crystal__target__target_name__iexact="+target)
+            .then(response => response.json())
+            .then(json => this.props.setTargetOn(json["results"][0].target_name))
+            .catch((error) => {
+                    // this.deployErrorModal(error);
+                })
+    }
+
+    componentDidMount() {
+        this.updateTarget()
+    }
+
+    componentDidUpdate(){
+        this.updateTarget()
     }
 
     render() {
@@ -24,8 +53,12 @@ class Fragspect extends Component {
 }
 
 function mapStateToProps(state) {
-  return {
-  }
+    return {}
+}
+const mapDispatchToProps = {
+    setTargetOn: apiActions.setTargetOn,
+    setTargetUnrecognised: apiActions.setTargetUnrecognised,
+    setErrorMessage: apiActions.setErrorMessage,
 }
 
-export default connect(mapStateToProps)(Fragspect)
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Fragspect))
