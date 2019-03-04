855e29b175d4568d4da9d89b718f905091e3ee62
Ric Gillams <ric.gillams@gmail.com>
2019-03-04 10:59:52 +0000

    landing launcher for fragspect

--- js/components/landing.js
+++ js/components/landing.js
@@ -47,7 +47,7 @@ export class Welcome extends React.Component {
                         <h3>Insert target name and press enter to launch Fragspect*:</h3>
                         <input id="fragspect_launch" key="fragspectLauncher" style={{width: 250}} defaultValue="insert target name here" onKeyDown={this.handleFragspectLaunch}></input>
                         <p>*this is only available to users who are registered for the visit in question.</p>
-                        <p>Please ensure you are logged in above and contact a beamline scientist, with the details of the target and visit, if you believe you should be included on a visit.</p>
+                        <p>Please ensure you are logged in and contact a beamline scientist, with the details of the target and visit, if you believe you should be included on a visit.</p>
                     </Row>
                     <Row style={{height: window.innerHeight * 0.1.toString() + "px"}}></Row>
                     <Row>
