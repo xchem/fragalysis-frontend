3ff11136639252d621f6f6f9d12f7a3570a73e6c
Ric Gillams <ric.gillams@gmail.com>
2019-03-06 14:55:20 +0000

    fragspect loading screen

--- js/components/modalFragspectLoading.js
+++ js/components/modalFragspectLoading.js
@@ -12,15 +12,15 @@ const customStyles = {
         backgroundColor: 'rgba(0, 0, 0, 0.85)'
     },
     content : {
-        top: '5%',
-        left: '5%',
-        right: '5%',
-        bottom: '5%',
+        top: '10%',
+        left: '10%',
+        right: '10%',
+        bottom: '10%',
         marginRight: '-20%',
-        transform: 'translate(-50%, -50%)',
+        transform: 'translate(0%, 0%)',
         border: '10px solid #7a7a7a',
-        width: '90%',
-        height:'90%'
+        width: '80%',
+        height:'80%'
     }
 };
 
