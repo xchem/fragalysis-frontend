ecee1cb4f539e3e673eaa5f08ebe6233c5121439
reskyner <reskyner@googlemail.com>
2019-05-24 16:30:29 +0100

    try new layout

--- js/components/moleculeView.js
+++ js/components/moleculeView.js
@@ -196,7 +196,7 @@ class MoleculeView extends GenericView {
                                     display: "inline-flex"
                                     }
         this.current_style = this.state.isToggleOn || this.state.complexOn ? selected_style : not_selected_style;
-        return <div className="container-fluid">
+        return <div className="container">
             <div className="row">
                 <div className="col-md-1">
                     <ButtonToolbar>
