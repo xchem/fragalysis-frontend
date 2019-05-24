8030a59f484cfdfe9e1a19189aa6bd66b1f3dd90
reskyner <reskyner@googlemail.com>
2019-05-24 11:38:45 +0100

    try new layout

--- js/components/generalComponents.js
+++ js/components/generalComponents.js
@@ -172,7 +172,7 @@ export class GenericView extends React.Component {
         this.old_url = ''
         this.state = {
             isToggleOn: false,
-            img_data: '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="20px" height="20px"><g>' +
+            img_data: '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="50px" height="50px"><g>' +
             '<circle cx="50" cy="0" r="5" transform="translate(5 5)"/>' +
             '<circle cx="75" cy="6.6987298" r="5" transform="translate(5 5)"/> ' +
             '<circle cx="93.3012702" cy="25" r="5" transform="translate(5 5)"/> ' +
