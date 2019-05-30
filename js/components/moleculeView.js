7ec877f3cbc4be8319dd3aa3319baa9b30a4be37
reskyner <reskyner@googlemail.com>
2019-05-30 16:19:55 +0100

    add properties table eg

--- js/components/moleculeView.js
+++ js/components/moleculeView.js
@@ -209,7 +209,22 @@ class MoleculeView extends GenericView {
                     {/*<RefinementOutcome data={this.props.data}></RefinementOutcome>*/}
                 </div>
                 <div className="col-md-5">
-                    Some example text
+                    <table>
+                        <tr>
+                            <th>MW</th>
+                            <th>logP</th>
+                            <th>TPSA</th>
+                            <th>HA</th>
+                            <th>Hacc</th>
+                        </tr>
+                        <tr>
+                            <td>{this.props.data.mw}</td>
+                            <td>{this.props.data.logp}</td>
+                            <td>{this.props.data.tpsa}</td>
+                            <td>{this.props.data.ha}</td>
+                            <td>{this.props.data.hacc}</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
