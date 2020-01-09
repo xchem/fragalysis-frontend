76d9022975db1ab61bddf9fd716e6b32e3ab0b8d
Pavol Brunclik <pavol.brunclik@m2ms.sk>
2020-01-09 11:08:04 +0100

    migrate babel to version 7

--- js/components/preview/viewerControls/index.js
+++ js/components/preview/viewerControls/index.js
@@ -2,10 +2,12 @@
  * Created by ricgillams on 28/06/2018.
  */
 
-import React, { memo, useState } from 'react';
+import React, { memo, Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Button } from '../../common/Inputs/Button';
+import { Panel } from '../../common';
 import { Settings, Mouse, PersonalVideo } from '@material-ui/icons';
-import { ButtonGroup, Grid, makeStyles, Tooltip } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import { SettingControls } from './settingsControls';
 import DisplayControls from './displayControls/';
 import { MouseControls } from './mouseControls';
@@ -18,15 +20,8 @@ const drawers = {
 
 const initDrawers = { [drawers.settings]: false, [drawers.display]: false, [drawers.mouse]: false };
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    padding: theme.spacing(1)
-  }
-}));
-
-export const ViewerControls = memo(({}) => {
+const ViewerControls = memo(({}) => {
   const [drawerSettings, setDrawerSettings] = useState(JSON.parse(JSON.stringify(initDrawers)));
-  const classes = useStyles();
 
   const openDrawer = key => {
     //close all and open selected by key
@@ -39,41 +34,39 @@ export const ViewerControls = memo(({}) => {
   };
 
   return (
-    <>
-      <Grid container justify="center">
-        <Grid item>
-          <ButtonGroup variant="contained" color="primary">
-            <Tooltip title="Settings controls">
-              <Button
-                size="small"
-                color="primary"
-                onClick={() => openDrawer(drawers.settings)}
-                className={classes.button}
-              >
-                <Settings />
-              </Button>
-            </Tooltip>
-            <Tooltip title="Display controls">
-              <Button
-                size="small"
-                color="primary"
-                onClick={() => openDrawer(drawers.display)}
-                className={classes.button}
-              >
-                <PersonalVideo />
-              </Button>
-            </Tooltip>
-            <Tooltip title="Mouse controls">
-              <Button size="small" color="primary" onClick={() => openDrawer(drawers.mouse)} className={classes.button}>
-                <Mouse />
-              </Button>
-            </Tooltip>
-          </ButtonGroup>
+    <Fragment>
+      <Panel hasHeader title="Viewer controls">
+        <Grid container justify="space-between" direction="row">
+          <Grid item>
+            <Button color="primary" onClick={() => openDrawer(drawers.settings)} startIcon={<Settings />}>
+              Settings
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button color="primary" onClick={() => openDrawer(drawers.display)} startIcon={<PersonalVideo />}>
+              Displayssssrssstr5ryuds
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button color="primary" onClick={() => openDrawer(drawers.mouse)} startIcon={<Mouse />}>
+              Mouse
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
+      </Panel>
       <SettingControls open={drawerSettings[drawers.settings]} onClose={closeAllDrawers} />
       <DisplayControls open={drawerSettings[drawers.display]} onClose={closeAllDrawers} />
       <MouseControls open={drawerSettings[drawers.mouse]} onClose={closeAllDrawers} />
-    </>
+    </Fragment>
   );
 });
+
+function mapStateToProps(state) {
+  return {
+    nglProtStyle: state.nglReducers.present.nglProtStyle
+  };
+}
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ViewerControls);
