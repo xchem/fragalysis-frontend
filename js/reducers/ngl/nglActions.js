6a5940e3c93acc98788c37e269c1bd4d679d4c0a
Tibor Postek <tibor.postek@m2ms.sk>
2020-01-15 10:37:04 +0100

    add unit tests of ngl actions

--- js/reducers/ngl/nglActions.js
+++ js/reducers/ngl/nglActions.js
@@ -5,6 +5,11 @@ import { CONSTANTS } from './nglConstants';
 
 export const loadNglObject = (target, representations) => ({ type: CONSTANTS.LOAD_OBJECT, target, representations });
 
+export const deleteNglObject = target => ({
+  type: CONSTANTS.DELETE_OBJECT,
+  target
+});
+
 export const updateComponentRepresentation = (objectInViewID, representationID, newRepresentation) => ({
   type: CONSTANTS.UPDATE_COMPONENT_REPRESENTATION,
   representationID,
@@ -24,13 +29,10 @@ export const removeComponentRepresentation = (objectInViewID, representationID) 
   objectInViewID
 });
 
-export const deleteNglObject = target => ({
-  type: CONSTANTS.DELETE_OBJECT,
-  target
-});
-
-export const setNglViewParams = (key, value, stage) => {
-  stage.setParameters({ [key]: value });
+export const setNglViewParams = (key, value, stage = undefined) => {
+  if (stage) {
+    stage.setParameters({ [key]: value });
+  }
   return {
     type: CONSTANTS.SET_NGL_VIEW_PARAMS,
     key,
@@ -38,9 +40,9 @@ export const setNglViewParams = (key, value, stage) => {
   };
 };
 
-export const setProteinLoadingState = hasLoad => ({ type: CONSTANTS.SET_PROTEINS_HAS_LOADED, payload: hasLoad });
-
 export const setNglOrientation = (orientation, div_id) => ({ type: CONSTANTS.SET_ORIENTATION, orientation, div_id });
+
+export const setProteinLoadingState = hasLoaded => ({ type: CONSTANTS.SET_PROTEINS_HAS_LOADED, payload: hasLoaded });
 
 export const saveCurrentStateAsDefaultScene = () => ({ type: CONSTANTS.SAVE_NGL_STATE_AS_DEFAULT_SCENE });
 
@@ -48,13 +50,21 @@ export const saveCurrentStateAsSessionScene = () => ({ type: CONSTANTS.SAVE_NGL_
 
 export const resetStateToDefaultScene = () => ({ type: CONSTANTS.RESET_NGL_VIEW_TO_DEFAULT_SCENE });
 
-export const resetStateToSessionScene = () => ({ type: CONSTANTS.RESET_NGL_VIEW_TO_SESSION_SCENE });
+export const resetStateToSessionScene = sessionData => ({
+  type: CONSTANTS.RESET_NGL_VIEW_TO_SESSION_SCENE,
+  payload: sessionData
+});
 
-export const removeAllNglComponents = stage => ({ type: CONSTANTS.REMOVE_ALL_NGL_COMPONENTS, stage });
+export const removeAllNglComponents = (stage = undefined) => ({ type: CONSTANTS.REMOVE_ALL_NGL_COMPONENTS, stage });
 
 export const setCountOfRemainingMoleculeGroups = count => ({
   type: CONSTANTS.SET_COUNT_OF_REMAINING_MOLECULE_GROUPS,
   payload: count
+});
+
+export const decrementCountOfRemainingMoleculeGroups = decrementedCount => ({
+  type: CONSTANTS.DECREMENT_COUNT_OF_REMAINING_MOLECULE_GROUPS,
+  payload: decrementedCount
 });
 
 export const incrementCountOfPendingNglObjects = () => ({
@@ -63,9 +73,4 @@ export const incrementCountOfPendingNglObjects = () => ({
 
 export const decrementCountOfPendingNglObjects = () => ({
   type: CONSTANTS.DECREMENT_COUNT_OF_PENDING_NGL_OBJECTS
-});
-
-export const decrementCountOfRemainingMoleculeGroups = decrementedCount => ({
-  type: CONSTANTS.DECREMENT_COUNT_OF_REMAINING_MOLECULE_GROUPS,
-  payload: decrementedCount
 });
