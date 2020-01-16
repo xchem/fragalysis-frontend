2cd854a857b73bd6e36ed54afb68c9ef1190fdbc
Tibor Postek <tibor.postek@m2ms.sk>
2020-01-16 19:20:42 +0100

    add unit test for delete object from ngl view, set orientation, reload from scene

--- js/reducers/ngl/nglDispatchActions.test.js
+++ js/reducers/ngl/nglDispatchActions.test.js
@@ -2,20 +2,32 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import {
   decrementCountOfRemainingMoleculeGroupsWithSavingDefaultState,
+  deleteObject,
   loadObject,
+  reloadNglViewFromScene,
+  setOrientation,
   setProteinsHasLoaded
 } from './nglDispatchActions';
-import { CONSTANTS } from './nglConstants';
+import { CONSTANTS, SCENES } from './nglConstants';
 import { getAction, getActionType } from '../../utils/testUtils';
 import {
   decrementCountOfPendingNglObjects,
   decrementCountOfRemainingMoleculeGroups,
+  deleteNglObject,
   incrementCountOfPendingNglObjects,
   loadNglObject,
+  resetStateToDefaultScene,
+  resetStateToSessionScene,
   saveCurrentStateAsDefaultScene,
+  setNglOrientation,
   setProteinLoadingState
 } from './nglActions';
-import { OBJECT_TYPE } from '../../components/nglView/constants';
+import { OBJECT_TYPE, SELECTION_TYPE } from '../../components/nglView/constants';
+import {
+  removeFromComplexList,
+  removeFromFragmentDisplayList,
+  removeFromVectorOnList
+} from '../selection/selectionActions';
 const { fn } = jest;
 
 describe("testing ngl reducer's async actions", () => {
@@ -47,18 +59,65 @@ describe("testing ngl reducer's async actions", () => {
     // eslint-disable-next-line jest/no-test-return-statement
     return store
       .dispatch(loadObject(target, stage))
-      .then(result => {
-        expect(result.type).toBe(getActionType(loadNglObject));
+      .then(async result => {
+        expect(result.type).toBe(await getActionType(loadNglObject));
         expect(result.target).toStrictEqual(target);
         expect(result.representations.length).toBeGreaterThan(0);
       })
-      .finally(() => {
-        expect(getAction(store, incrementCountOfPendingNglObjects)).not.toBeNull();
-        expect(getAction(store, decrementCountOfPendingNglObjects)).not.toBeNull();
+      .finally(async () => {
+        expect(await getAction(store, incrementCountOfPendingNglObjects)).not.toBeNull();
+        expect(await getAction(store, decrementCountOfPendingNglObjects)).not.toBeNull();
       });
   });
 
-  // deleteObject
+  it('should delete object', async () => {
+    expect.hasAssertions();
+    let store = mockStore();
+    const targetLigand = {
+      selectionType: SELECTION_TYPE.LIGAND,
+      moleculeId: 1
+    };
+    const targetComplex = {
+      selectionType: SELECTION_TYPE.COMPLEX,
+      moleculeId: 2
+    };
+    const targetVector = {
+      selectionType: SELECTION_TYPE.VECTOR,
+      moleculeId: 3
+    };
+
+    const stage = {
+      loadFile: fn(() =>
+        Promise.resolve({
+          addRepresentation: fn(() => ({
+            uuid: null,
+            getParameters: fn(() => {}),
+            repr: { parameters: {} }
+          })),
+          autoView: fn()
+        })
+      ),
+      getComponentsByName: fn(() => ({
+        list: [1, 2, 3]
+      })),
+      removeComponent: fn(() => {})
+    };
+
+    await store.dispatch(deleteObject(targetLigand, stage, true));
+    expect(await getAction(store, removeFromFragmentDisplayList)).not.toBeNull();
+    store.clearActions();
+
+    await store.dispatch(deleteObject(targetComplex, stage, true));
+    expect(await getAction(store, removeFromComplexList)).not.toBeNull();
+    store.clearActions();
+
+    await store.dispatch(deleteObject(targetVector, stage, true));
+    expect(await getAction(store, removeFromVectorOnList)).not.toBeNull();
+    store.clearActions();
+
+    await store.dispatch(deleteObject(targetLigand, stage, false));
+    expect(await getAction(store, deleteNglObject)).not.toBeNull();
+  });
 
   it('should decrement count of remaining molecule groups in case with more than one remaining molecule groups', async () => {
     expect.hasAssertions();
@@ -120,6 +179,7 @@ describe("testing ngl reducer's async actions", () => {
       type: getActionType(setProteinLoadingState),
       payload: hasLoaded
     });
+    store.clearActions();
 
     withoutSavingToDefaultState = false;
     store.dispatch(setProteinsHasLoaded(hasLoaded, withoutSavingToDefaultState));
@@ -152,6 +212,7 @@ describe("testing ngl reducer's async actions", () => {
       type: getActionType(setProteinLoadingState),
       payload: hasLoaded
     });
+    store.clearActions();
 
     withoutSavingToDefaultState = false;
     store.dispatch(setProteinsHasLoaded(hasLoaded, withoutSavingToDefaultState));
@@ -163,7 +224,126 @@ describe("testing ngl reducer's async actions", () => {
     });
   });
 
-  // setOrientation
+  it('should set orientation', async () => {
+    expect.hasAssertions();
+    const orientation = {
+      elements: [1, 2, 3, 'df']
+    };
 
-  // reloadNglViewFromScene
+    const div_id = 'MAJOR_VIEW';
+
+    let store = mockStore({
+      nglReducers: {
+        present: {
+          nglOrientations: {
+            first: { elements: [35, 'g'] },
+            [div_id]: orientation,
+            third: { elements: [235, 'g3'] }
+          }
+        }
+      }
+    });
+
+    await store.dispatch(setOrientation(div_id, orientation));
+    expect(await store.getActions()).toHaveLength(0);
+
+    let storeWithAnotherOrientationElem = mockStore({
+      nglReducers: {
+        present: {
+          nglOrientations: {
+            first: { elements: [35, 'g'] },
+            [div_id]: { elements: [3534, 'wfsweg'] },
+            third: { elements: [235, 'g3'] }
+          }
+        }
+      }
+    });
+
+    await storeWithAnotherOrientationElem.dispatch(setOrientation(div_id, orientation));
+    expect(await getAction(storeWithAnotherOrientationElem, setNglOrientation)).not.toBeNull();
+
+    let storeWithoutOrientations = mockStore({
+      nglReducers: {
+        present: {
+          nglOrientations: undefined
+        }
+      }
+    });
+
+    await storeWithoutOrientations.dispatch(setOrientation(div_id, orientation));
+    expect(await getAction(storeWithoutOrientations, setNglOrientation)).not.toBeNull();
+
+    let storeWithNotAllOrientations = mockStore({
+      nglReducers: {
+        present: {
+          nglOrientations: {
+            first: { elements: [35, 'g'] },
+            second: { elements: [235, 'g3'] }
+          }
+        }
+      }
+    });
+    await storeWithNotAllOrientations.dispatch(setOrientation(div_id, orientation));
+    expect(await getAction(storeWithNotAllOrientations, setNglOrientation)).not.toBeNull();
+  });
+
+  it('should reload NGL View from session scene', async () => {
+    expect.hasAssertions();
+    let store = mockStore();
+
+    const stage = {
+      removeAllComponents: fn(),
+      viewerControls: {
+        orient: fn()
+      }
+    };
+
+    const display_div = 'MajorView';
+    const scene = SCENES.sessionScene;
+    const sessionData = {
+      nglReducers: {
+        present: {
+          [scene]: {
+            objectsInView: {},
+            viewParams: { a: 'ssfs', b: 'dfd' },
+            nglOrientations: {}
+          }
+        }
+      }
+    };
+    store.dispatch(reloadNglViewFromScene(stage, display_div, scene, sessionData));
+
+    expect(await getAction(store, resetStateToSessionScene)).toStrictEqual({
+      type: getActionType(resetStateToSessionScene),
+      payload: sessionData
+    });
+  });
+
+  it('should reload NGL View from default scene', async () => {
+    expect.hasAssertions();
+    const scene = SCENES.defaultScene;
+    let store = mockStore({
+      nglReducers: {
+        present: {
+          [scene]: {
+            objectsInView: {},
+            viewParams: { a: 'ssfs', b: 'dfd' },
+            nglOrientations: {}
+          }
+        }
+      }
+    });
+
+    const stage = {
+      removeAllComponents: fn(),
+      viewerControls: {
+        orient: fn()
+      }
+    };
+
+    const display_div = 'MajorView';
+    store.dispatch(reloadNglViewFromScene(stage, display_div, scene));
+
+    expect(await getAction(store, resetStateToDefaultScene)).not.toBeNull();
+  });
 });
