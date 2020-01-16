b3c61e7e08b6a659fa7863a57e440b580b5266f9
Tibor Postek <tibor.postek@m2ms.sk>
2020-01-16 16:35:36 +0100

    add unit test for loading object into ngl view

--- js/reducers/ngl/nglDispatchActions.test.js
+++ js/reducers/ngl/nglDispatchActions.test.js
@@ -2,19 +2,63 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import {
   decrementCountOfRemainingMoleculeGroupsWithSavingDefaultState,
+  loadObject,
   setProteinsHasLoaded
 } from './nglDispatchActions';
 import { CONSTANTS } from './nglConstants';
 import { getAction, getActionType } from '../../utils/testUtils';
 import {
+  decrementCountOfPendingNglObjects,
   decrementCountOfRemainingMoleculeGroups,
+  incrementCountOfPendingNglObjects,
+  loadNglObject,
   saveCurrentStateAsDefaultScene,
   setProteinLoadingState
 } from './nglActions';
+import { OBJECT_TYPE } from '../../components/nglView/constants';
+const { fn } = jest;
 
 describe("testing ngl reducer's async actions", () => {
   const middlewares = [thunk]; // add your middlewares like `redux-thunk`
   const mockStore = configureStore(middlewares);
+
+  it('should load object', () => {
+    expect.hasAssertions();
+    let store = mockStore();
+    const target = {
+      name: 'My protein',
+      OBJECT_TYPE: OBJECT_TYPE.PROTEIN,
+      property: { a: 'sdff' }
+    };
+
+    const stage = {
+      loadFile: fn(() =>
+        Promise.resolve({
+          addRepresentation: fn(() => ({
+            uuid: null,
+            getParameters: fn(() => {}),
+            repr: { parameters: {} }
+          })),
+          autoView: fn()
+        })
+      )
+    };
+
+    // eslint-disable-next-line jest/no-test-return-statement
+    return store
+      .dispatch(loadObject(target, stage))
+      .then(result => {
+        expect(result.type).toBe(getActionType(loadNglObject));
+        expect(result.target).toStrictEqual(target);
+        expect(result.representations.length).toBeGreaterThan(0);
+      })
+      .finally(() => {
+        expect(getAction(store, incrementCountOfPendingNglObjects)).not.toBeNull();
+        expect(getAction(store, decrementCountOfPendingNglObjects)).not.toBeNull();
+      });
+  });
+
+  // deleteObject
 
   it('should decrement count of remaining molecule groups in case with more than one remaining molecule groups', async () => {
     expect.hasAssertions();
@@ -118,4 +162,8 @@ describe("testing ngl reducer's async actions", () => {
       payload: hasLoaded
     });
   });
+
+  // setOrientation
+
+  // reloadNglViewFromScene
 });
