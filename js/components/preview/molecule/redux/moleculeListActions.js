b727fba523a5510e70c3ed7cac9deca3422c7c5c
Tibor Postek <tibor.postek@m2ms.sk>
2020-01-21 08:39:03 +0100

    create selector for molecule list

--- js/components/preview/molecule/redux/moleculeListActions.js
+++ js/components/preview/molecule/redux/moleculeListActions.js
@@ -1,22 +1,27 @@
 import { createSelector } from 'reselect';
 
-// concat molecule results for all selected molecule groups into single list
-export const getJoinedMoleculeList = obj_selection => (dispatch, getState) => {
-  const state = getState();
-  const cached_mol_lists = state.apiReducers.present.cached_mol_lists;
-  const mol_group_list = state.apiReducers.present.mol_group_list;
+const getCachedMoleculeLists = state => state.apiReducers.present.cached_mol_lists;
+const getMoleculeGroupLists = state => state.apiReducers.present.mol_group_list;
+const getMoleculeGroupSelection = state => state.selectionReducers.present.mol_group_selection;
+const getObjectSelection = state => state.selectionReducers.present.object_selection;
 
-  const object_selection = obj_selection || state.selectionReducers.present.mol_group_selection;
-
-  let joinedMoleculeLists = [];
-  if (object_selection) {
-    object_selection.forEach(obj => {
-      const cachedData = cached_mol_lists[obj];
-      const site = (mol_group_list || []).findIndex(group => group.id === obj) + 1;
-      if (cachedData && cachedData.results) {
-        cachedData.results.forEach(r => joinedMoleculeLists.push(Object.assign({ site: site }, r)));
-      }
-    });
+export const getJoinedMoleculeList = createSelector(
+  getCachedMoleculeLists,
+  getMoleculeGroupLists,
+  getMoleculeGroupSelection,
+  getObjectSelection,
+  (cached_mol_lists, mol_group_list, mol_group_selection, obj_selection) => {
+    const object_selection = obj_selection || mol_group_selection;
+    let joinedMoleculeLists = [];
+    if (object_selection) {
+      object_selection.forEach(obj => {
+        const cachedData = cached_mol_lists[obj];
+        const site = (mol_group_list || []).findIndex(group => group.id === obj) + 1;
+        if (cachedData && cachedData.results) {
+          cachedData.results.forEach(r => joinedMoleculeLists.push(Object.assign({ site: site }, r)));
+        }
+      });
+    }
+    return joinedMoleculeLists;
   }
-  return joinedMoleculeLists;
-};
+);
