6731f4da2e10a02344aee388262f14fe2d85741f
Tibor Postek <tibor.postek@m2ms.sk>
2020-01-24 15:53:08 +0100

    add not completed movement of business logic from summaryView to dispatch actions

--- js/components/preview/summary/redux/dispatchAction.js
+++ js/components/preview/summary/redux/dispatchAction.js
@@ -2,6 +2,9 @@ import { api } from '../../../../utils/api';
 import { resetCompoundImage, setCompoundImage, setIsLoadingCompoundImage, setOldUrl } from './actions';
 import { base_url } from '../../../routes/constants';
 import { isEmpty } from 'lodash';
+import JSZip from 'jszip';
+import FileSaver from 'file-saver';
+import { DockingScripts } from '../../../../utils/script_utils';
 
 const fetchCompoundImage = url => (dispatch, getState) => {
   const state = getState();
@@ -64,4 +67,47 @@ export const reloadSummaryCompoundImage = ({ currentVector, bondColorMap }) => (
     dispatch(resetCompoundImage());
     return Promise.resolve('Resetting of compound image');
   }
+};
+
+export const downloadAsYankDuck = () => async (dispatch, getState) => {
+  const state = getState();
+  const duck_yank_data = state.apiReducers.present.duck_yank_data;
+  if (!isEmpty(duck_yank_data)) {
+    const dockingScripts = new DockingScripts();
+    let interaction = duck_yank_data['interaction'];
+    let complex_id = duck_yank_data['complex_id'];
+    let url = window.location.protocol + '//' + window.location.host + '/api/molecules/' + complex_id.toString() + '/';
+    const mol_response = await api({ url }).catch(error => {
+      return Promise.reject(error);
+    });
+    const mol_json = await mol_response.data;
+    let prot_id = mol_json['prot_id'];
+    url = window.location.protocol + '//' + window.location.host + '/api/protpdb/' + prot_id + '/';
+    const prot_response = await api({ url }).catch(error => {
+      return Promise.reject(error);
+    });
+    const prot_json = await prot_response.data;
+    url = window.location.protocol + '//' + window.location.host + '/api/proteins/' + prot_id + '/';
+    const prot_data_response = await api({ url }).catch(error => {
+      return Promise.reject(error);
+    });
+    const prot_data_json = await prot_data_response.data;
+    const prot_code = prot_data_json['code'];
+    const pdb_data = prot_json['pdb_data'];
+    const mol_data = mol_json['sdf_info'];
+    // Turn these into server side
+    let duck_yaml = dockingScripts.getDuckYaml(prot_code, interaction);
+    let yank_yaml = dockingScripts.getYankYaml(prot_code);
+    let f_name = prot_code + '_' + interaction + '_duck_yank';
+    let zip = new JSZip();
+    let tot_folder = zip.folder(f_name);
+    tot_folder.file('duck.yaml', duck_yaml);
+    tot_folder.file('yank.yaml', yank_yaml);
+    tot_folder.file(prot_code + '.mol', mol_data);
+    tot_folder.file(prot_code + '_apo.pdb', pdb_data);
+    const content = await zip.generateAsync({ type: 'blob' });
+    FileSaver.saveAs(content, f_name + '.zip');
+    return Promise.resolve();
+  }
+  return Promise.reject('Missing duck_yank_data');
 };
