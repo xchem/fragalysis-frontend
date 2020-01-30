4d3b9310df095eae2b8d259ea1f21347d17bfc0e
Tibor Postek <tibor.postek@m2ms.sk>
2020-01-30 12:35:30 +0100

    fix loading image for compounds

--- js/components/preview/compounds/redux/reducer.js
+++ js/components/preview/compounds/redux/reducer.js
@@ -1,21 +1,9 @@
 import { constants } from './constants';
 
-export const loadingCompoundImage =
-  '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="80px" height="80px"><g>' +
-  '<circle cx="50" cy="0" r="5" transform="translate(5 5)"/>' +
-  '<circle cx="75" cy="6.6987298" r="5" transform="translate(5 5)"/> ' +
-  '<circle cx="93.3012702" cy="25" r="5" transform="translate(5 5)"/> ' +
-  '<circle cx="100" cy="50" r="5" transform="translate(5 5)"/> ' +
-  '<circle cx="93.3012702" cy="75" r="5" transform="translate(5 5)"/> ' +
-  '<circle cx="75" cy="93.3012702" r="5" transform="translate(5 5)"/> ' +
-  '<circle cx="50" cy="100" r="5" transform="translate(5 5)"/> ' +
-  '<circle cx="25" cy="93.3012702" r="5" transform="translate(5 5)"/> ' +
-  '<circle cx="6.6987298" cy="75" r="5" transform="translate(5 5)"/> ' +
-  '<circle cx="0" cy="50" r="5" transform="translate(5 5)"/> ' +
-  '<circle cx="6.6987298" cy="25" r="5" transform="translate(5 5)"/> ' +
-  '<circle cx="25" cy="6.6987298" r="5" transform="translate(5 5)"/> ' +
-  '<animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 55 55" to="360 55 55" dur="3s" repeatCount="indefinite" /> </g> ' +
-  '</svg>';
+export const loadingCompoundImage = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="25px" height="25px"><g>
+  <circle cx="50" cy="50" fill="none" stroke="#3f51b5" stroke-width="4" r="26" stroke-dasharray="150.79644737231007 52.26548245743669" transform="rotate(238.988 50 50)">
+    <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="0.689655172413793s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform>
+  </circle>  '</svg>`;
 
 export const INITIAL_STATE = {
   currentPage: -1,
--- js/reducers/selection/dispatchActions.js
+++ js/reducers/selection/dispatchActions.js
@@ -1,7 +1,9 @@
-import { resetCurrentCompoundsSettings } from '../../components/preview/compounds/redux/actions';
+import { resetCurrentCompoundsSettings, setCurrentCompounds } from '../../components/preview/compounds/redux/actions';
 import { selectVector } from './selectionActions';
+import { getAllCompoundsList } from './selectors';
 
-export const selectVectorAndResetCompounds = currentVector => async dispatch => {
+export const selectVectorAndResetCompounds = currentVector => async (dispatch, getState) => {
   await dispatch(resetCurrentCompoundsSettings());
   dispatch(selectVector(currentVector));
+  dispatch(setCurrentCompounds(getAllCompoundsList(getState())));
 };
