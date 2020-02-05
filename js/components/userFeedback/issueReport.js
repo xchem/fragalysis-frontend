fdb5b04b140dde9e40575ecad0532162cbc8b9af
matej <matej.vavrek@m2ms.sk>
2020-02-05 12:48:39 +0100

    #3 first draft of issue reporting

--- js/components/userFeedback/issueReport.js
+++ js/components/userFeedback/issueReport.js
@@ -2,9 +2,74 @@
  * This component creates a button for reporting new issues and handling them.
  */
 
-import React, { memo } from 'react';
-import { ReportForm, FORM_TYPE } from './reportForm';
+import React, { memo, useState } from 'react';
+import { makeStyles, TextField } from '@material-ui/core';
+import { ReportProblem } from '@material-ui/icons';
+import { Button } from '../common/Inputs/Button';
+import Modal from '../common/Modal';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1)
+  },
+  input: {
+    display: 'none'
+  }
+}));
 
 export const IssueReport = memo(() => {
-  return <ReportForm formType={FORM_TYPE.ISSUE} />;
+  const classes = useStyles();
+
+  const createIssue = () => {
+    console.log('creating new issue');
+    // ReactDOM.render(<UserFeedbackForm/>, document.getElementById('root'));
+  };
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button
+        startIcon={<ReportProblem />}
+        variant="text"
+        size="small"
+        className={classes.button}
+        onClick={handleOpen}
+        >
+        Report issue
+      </Button>
+      <Modal open={open} onClose={handleClose} >
+        <div>
+          <h3>Report issue</h3>
+          <TextField id="issue-name" label="Name" />
+          <br /><TextField id="issue-email" label="Email" />
+          <br /><TextField id="issue-title" label="Title" />
+          <br /><TextField
+            id="issue-description"
+            label="Description"
+            placeholder="Describe your problem in a detail."
+            multiline
+            rows="4"
+          />
+          <br /><Button
+            color="primary"
+            onClick={createIssue}
+          >
+            Report issue
+          </Button>
+          <Button onClick={handleClose}>
+            Close
+          </Button>
+        </div>
+      </Modal>
+    </div>
+  );
 });
