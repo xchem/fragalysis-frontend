176d9d1a8c70c250aa6edde501050500035ba8d0
Tibor Postek <tibor.postek@m2ms.sk>
2020-03-02 11:15:46 +0100

    #144 fix shift + click

--- js/components/preview/compounds/redux/dispatchActions.js
+++ js/components/preview/compounds/redux/dispatchActions.js
@@ -100,7 +100,7 @@ const showCompoundNglView = ({ majorViewStage, data, index }) => (dispatch, getS
         'X-Requested-With': 'XMLHttpRequest',
         'X-CSRFToken': getCsrfToken()
       },
-      body: JSON.stringify(post_data)
+      data: JSON.stringify(post_data)
     })
       .then(response => {
         // Now load this into NGL
