8f28d960176d686d3b6b92977e64b2b117bf3cab
Matej Vavrek <matej.vavrek@m2ms.sk>
2020-03-05 13:30:15 +0100

    #148 added hover styling for selected molecule options

--- js/components/preview/molecule/moleculeView.js
+++ js/components/preview/molecule/moleculeView.js
@@ -48,10 +48,19 @@ const useStyles = makeStyles(theme => ({
   },
   contColButtonSelected: {
     backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText
+    color: theme.palette.primary.contrastText,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.light,
+      color: theme.palette.black
+    }
   },
   contColButtonHalfSelected: {
-    backgroundColor: theme.palette.primary.semidark
+    backgroundColor: theme.palette.primary.semidark,
+    color: theme.palette.primary.contrastText,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.light,
+      color: theme.palette.black
+    }
   },
   detailsCol: {
     border: 'solid 1px',
