207eca619eb17b0f5021753f5faec02fdeb9daf3
Tibor Postek <tibor.postek@m2ms.sk>
2020-04-22 14:53:54 +0200

    #276 move share button

--- js/components/preview/projectHistory/index.js
+++ js/components/preview/projectHistory/index.js
@@ -1,27 +1,15 @@
 import React, { memo, useContext, useEffect, useRef } from 'react';
 import { Panel } from '../../common/Surfaces/Panel';
-import {
-  Grid,
-  IconButton,
-  Typography,
-  Table,
-  TableBody,
-  TableRow,
-  TableCell,
-  TableHead,
-  makeStyles
-} from '@material-ui/core';
 import { templateExtend, TemplateName, Orientation, Gitgraph } from '@gitgraph/react';
-import { MergeType, Share } from '@material-ui/icons';
+import { MergeType } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core';
 import { Button } from '../../common/Inputs/Button';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useRouteMatch } from 'react-router-dom';
-import { base_url, URLS } from '../../routes/constants';
 import { loadSnapshotTree } from '../../projects/redux/dispatchActions';
 import palette from '../../../theme/palette';
 import { ModalShareSnapshot } from '../../snapshot/modals/modalShareSnapshot';
-import { setIsOpenModalBeforeExit, setSelectedSnapshotToSwitch, setSharedSnapshot } from '../../snapshot/redux/actions';
-import { resetReducersBetweenSnapshots, switchBetweenSnapshots } from '../redux/dispatchActions';
+import { setIsOpenModalBeforeExit, setSelectedSnapshotToSwitch } from '../../snapshot/redux/actions';
 import { NglContext } from '../../nglView/nglProvider';
 
 export const heightOfProjectHistory = '164px';
@@ -86,11 +74,7 @@ export const ProjectHistory = memo(({ setHeight, showFullHistory }) => {
   let match = useRouteMatch();
   const projectID = match && match.params && match.params.projectId;
   const snapshotId = match && match.params && match.params.snapshotId;
-
-  const currentProjectID = useSelector(state => state.projectReducers.currentProject.projectID);
   const currentSnapshotID = useSelector(state => state.projectReducers.currentSnapshot.id);
-  const currentSnapshotTitle = useSelector(state => state.projectReducers.currentSnapshot.title);
-  const currentSnapshotDescription = useSelector(state => state.projectReducers.currentSnapshot.description);
   const currentSnapshotList = useSelector(state => state.projectReducers.currentSnapshotList);
   const currentSnapshotTree = useSelector(state => state.projectReducers.currentSnapshotTree);
   const isLoadingTree = useSelector(state => state.projectReducers.isLoadingTree);
@@ -146,23 +130,6 @@ export const ProjectHistory = memo(({ setHeight, showFullHistory }) => {
         hasHeader
         title="Project History"
         headerActions={[
-          <Button
-            color="inherit"
-            variant="text"
-            size="small"
-            startIcon={<Share />}
-            onClick={() => {
-              dispatch(
-                setSharedSnapshot({
-                  title: currentSnapshotTitle,
-                  description: currentSnapshotDescription,
-                  url: `${base_url}${URLS.projects}${currentProjectID}/${currentSnapshotID}`
-                })
-              );
-            }}
-          >
-            Share
-          </Button>,
           <Button color="inherit" variant="text" size="small" onClick={showFullHistory} startIcon={<MergeType />}>
             Detail
           </Button>
@@ -202,31 +169,6 @@ export const ProjectHistory = memo(({ setHeight, showFullHistory }) => {
               </Gitgraph>
             )}
         </div>
-        {/*<Grid item>*/}
-        {/*  <Table>*/}
-        {/*    <TableHead>*/}
-        {/*      <TableRow>*/}
-        {/*        <TableCell>Title</TableCell>*/}
-        {/*        <TableCell align="right">Author</TableCell>*/}
-        {/*        <TableCell align="right">Created</TableCell>*/}
-        {/*      </TableRow>*/}
-        {/*    </TableHead>*/}
-        {/*    <TableBody>*/}
-        {/*      <TableRow>*/}
-        {/*        <TableCell component="th" scope="row">*/}
-        {/*          {snapshotDetail.name}*/}
-        {/*        </TableCell>*/}
-        {/*        <TableCell align="right">*/}
-        {/*          {snapshotDetail.author && snapshotDetail.author.username},*/}
-        {/*          {snapshotDetail.author && snapshotDetail.author.email}*/}
-        {/*        </TableCell>*/}
-        {/*        <TableCell align="right">*/}
-        {/*          {snapshotDetail.created && moment(snapshotDetail.created).format('LLL')}*/}
-        {/*        </TableCell>*/}
-        {/*      </TableRow>*/}
-        {/*    </TableBody>*/}
-        {/*  </Table>*/}
-        {/*</Grid>*/}
       </Panel>
       <ModalShareSnapshot />
     </>
