fb30269de96f5f4e3e8d53333e89209e984fd3e5
Tibor Postek <tibor.postek@m2ms.sk>
2020-06-09 10:01:35 +0200

    #273 close inspiration dialog after click on another tab

--- js/components/datasets/selectedCompoundsList.js
+++ js/components/datasets/selectedCompoundsList.js
@@ -1,10 +1,12 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useEffect, useRef, useState } from 'react';
 import { Panel } from '../common/Surfaces/Panel';
 import { CircularProgress, Grid, makeStyles } from '@material-ui/core';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getMoleculesObjectIDListOfCompoundsToBuy } from './redux/selectors';
 import InfiniteScroll from 'react-infinite-scroller';
 import { DatasetMoleculeView } from './datasetMoleculeView';
+import { InspirationDialog } from './inspirationDialog';
+import { setIsOpenInspirationDialog } from './redux/actions';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -27,8 +29,12 @@ export const SelectedCompoundList = memo(({ height }) => {
   const imgHeight = 34;
   const imgWidth = 150;
   const moleculesPerPage = 5;
+  const dispatch = useDispatch();
   const [currentPage, setCurrentPage] = useState(0);
   const moleculesObjectIDListOfCompoundsToBuy = useSelector(getMoleculesObjectIDListOfCompoundsToBuy);
+  const isOpenInspirationDialog = useSelector(state => state.datasetsReducers.isOpenInspirationDialog);
+  const [selectedInspirationMoleculeRef, setSelectedInspirationMoleculeRef] = useState(null);
+  const inspirationDialogRef = useRef();
 
   const loadNextMolecules = () => {
     setCurrentPage(currentPage + 1);
@@ -38,8 +44,22 @@ export const SelectedCompoundList = memo(({ height }) => {
   const currentMolecules = moleculesObjectIDListOfCompoundsToBuy.slice(0, listItemOffset);
   const canLoadMore = listItemOffset < moleculesObjectIDListOfCompoundsToBuy.length;
 
+  useEffect(() => {
+    return () => {
+      dispatch(setIsOpenInspirationDialog(false));
+    };
+  }, [dispatch]);
+
   return (
     <Panel hasHeader title="Selected Compounds" withTooltip>
+      {isOpenInspirationDialog && (
+        <InspirationDialog
+          open
+          anchorEl={selectedInspirationMoleculeRef}
+          //    datasetID={datasetID}
+          ref={inspirationDialogRef}
+        />
+      )}
       <Grid container direction="column" justify="flex-start" className={classes.container} style={{ height: height }}>
         {currentMolecules.length > 0 && (
           <Grid item className={classes.gridItemList}>
@@ -69,6 +89,7 @@ export const SelectedCompoundList = memo(({ height }) => {
                   imageWidth={imgWidth}
                   data={data.molecule}
                   datasetID={data.datasetID}
+                  setRef={setSelectedInspirationMoleculeRef}
                 />
               ))}
             </InfiniteScroll>
