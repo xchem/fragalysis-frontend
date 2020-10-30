bcee1f3a4584e2689e855f3ecf815a7753ab679c
Adriána Kohanová <adriana.kohanova@m2ms.sk>
2020-10-30 09:15:19 +0100

    #430 Visualize users action history

--- js/components/tracking/trackingModal.js
+++ js/components/tracking/trackingModal.js
@@ -1,21 +1,35 @@
 import React, { memo } from 'react';
 import { useSelector } from 'react-redux';
 import Modal from '../common/Modal';
-import { makeStyles, Typography } from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 import { Timeline, TimelineEvent } from 'react-event-timeline';
-import { Check } from '@material-ui/icons';
+import { Check, Clear } from '@material-ui/icons';
 import palette from '../../theme/palette';
+import { Panel } from '../common';
 
 const useStyles = makeStyles(theme => ({
   customModal: {
     width: '70%',
-    height: '90%',
-    overflow: 'scroll'
+    height: '90%'
+  },
+  customContentModal: {
+    height: '100%'
   },
   timelineEvent: {
     borderBottom: '1px dashed ' + palette.divider,
     paddingBottom: '10px'
-  }
+  },
+  divContainer: {
+    height: '100%',
+    width: '100%',
+    paddingTop: theme.spacing(1) / 2
+  },
+  divScrollable: {
+    height: '100%',
+    width: '100%',
+    overflow: 'auto'
+  },
+  containerExpanded: { height: '100%' }
 }));
 
 export const TrackingModal = memo(({ openModal, onModalClose }) => {
@@ -28,20 +42,32 @@ export const TrackingModal = memo(({ openModal, onModalClose }) => {
   }
 
   return (
-    <Modal otherClasses={classes.customModal} open={openModal} onClose={() => onModalClose()}>
-      <Typography variant="h5">Action list</Typography>
-      <Timeline>
-        {actionList.map((data, index) => (
-          <TimelineEvent
-            key={index}
-            title={data.text}
-            createdAt={new Date(data.timestamp).toLocaleString()}
-            icon={<Check />}
-            iconColor={palette.primary.main}
-            className={classes.timelineEvent}
-          ></TimelineEvent>
-        ))}
-      </Timeline>
+    <Modal
+      otherClasses={classes.customModal}
+      otherContentClasses={classes.customContentModal}
+      open={openModal}
+      onClose={() => onModalClose()}
+    >
+      <Panel bodyOverflow={true} hasHeader={true} title="Action List">
+        <Grid container justify="space-between" className={classes.containerExpanded}>
+          <div className={classes.divContainer}>
+            <div className={classes.divScrollable}>
+              <Timeline>
+                {actionList.map((data, index) => (
+                  <TimelineEvent
+                    key={index}
+                    title={data.text}
+                    createdAt={new Date(data.timestamp).toLocaleString()}
+                    icon={data.type.includes('OFF') === true ? <Clear /> : <Check />}
+                    iconColor={palette.primary.main}
+                    className={classes.timelineEvent}
+                  ></TimelineEvent>
+                ))}
+              </Timeline>
+            </div>
+          </div>
+        </Grid>
+      </Panel>
     </Modal>
   );
 });
