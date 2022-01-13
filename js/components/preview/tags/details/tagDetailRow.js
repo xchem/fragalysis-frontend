25ff8104e0961d1f2f250b8fb6898bd66455300d
Anton Gasperak <anton.gasperak@m2ms.sk>
2022-01-13 09:47:09 +0100

    #763 - Initial layout change

--- js/components/preview/tags/details/tagDetailRow.js
+++ js/components/preview/tags/details/tagDetailRow.js
@@ -45,13 +45,11 @@ const useStyles = makeStyles(theme => ({
     }
   },
   divContainer: {
-    flexDirection: 'row',
-    display: 'flex',
-    // height: '100%',
-    width: '100%'
-    /*paddingTop: theme.spacing(1) / 2,
-    marginRight: '1px',
-    marginLeft: '1px'*/
+    width: '100%',
+    display: 'grid',
+    gridTemplateColumns: '1fr 75px 75px min-content 20px min-content min-content',
+    alignItems: 'center',
+    gap: theme.spacing()
   },
   editButton: {
     backgroundColor: theme.palette.success.light,
@@ -64,10 +62,6 @@ const useStyles = makeStyles(theme => ({
   editButtonIcon: {
     width: '0.75em',
     height: '0.75em'
-  },
-  unselectButtonWrapper: {
-    paddingLeft: '1px !important',
-    paddingRight: '1px !important'
   }
 }));
 
@@ -139,103 +133,86 @@ const TagDetailRow = memo(({ tag, moleculesToEditIds, moleculesToEdit }) => {
   };
 
   return (
-    <Grid container item className={classes.divContainer} spacing={1} wrap="nowrap" alignItems="center" xs={12}>
+    <div className={classes.divContainer}>
       {/* TagView Chip */}
-      <Grid item xs={3}>
-        <TagView
-          key={`tag-item-editor${tag.id}`}
-          tag={tag}
-          selected={selectedTagList.some(i => i.id === tag.id)}
-          handleClick={handleTagClick}
-          // disabled={!DJANGO_CONTEXT.pk}
-          disabled={false}
-          isEdit={true}
-          isTagEditor={true}
-        ></TagView>
-      </Grid>
+      <TagView
+        key={`tag-item-editor${tag.id}`}
+        tag={tag}
+        selected={selectedTagList.some(i => i.id === tag.id)}
+        handleClick={handleTagClick}
+        // disabled={!DJANGO_CONTEXT.pk}
+        disabled={false}
+        isEdit={true}
+        isTagEditor={true}
+      ></TagView>
       {/* category */}
-      <Grid item xs={1} wrap="nowrap">
-        <Tooltip title={CATEGORY_TYPE_BY_ID[tag.category_id]}>
-          <Typography variant="body2" noWrap>
-            {CATEGORY_TYPE_BY_ID[tag.category_id]}
-          </Typography>
-        </Tooltip>
-      </Grid>
-      {/* select hits button */}
-      <Grid item xs={2} className={hasSelectedMolecule() ? classes.unselectButtonWrapper : null}>
-        <Tooltip title="Select hits">
-          <Grid item container justify="center">
-            <Button
-              variant="outlined"
-              className={classNames(classes.contColButton, {
-                [classes.contColButtonSelected]: hasSelectedMolecule(),
-                [classes.contColButtonHalfSelected]: false
-              })}
-              onClick={() => handleSelectHits()}
-              disabled={false}
-            >
-              {hasSelectedMolecule() ? 'Unselect hits' : 'Select hits'}
-            </Button>
-          </Grid>
-        </Tooltip>
-      </Grid>
-      {/* discourse button */}
-      <Grid item xs={2}>
-        <Tooltip title="Discourse link">
-          <Grid item container justify="center">
-            <Button
-              variant="outlined"
-              className={classNames(classes.contColButton, {
-                [classes.contColButtonSelected]: false,
-                [classes.contColButtonHalfSelected]: false
-              })}
-              onClick={() => {
-                if (isURL(tag.discourse_url)) {
-                  window.open(tag.discourse_url, '_blank');
-                } else {
-                  createTagPost(tag, targetName, getDefaultTagDiscoursePostText(tag)).then(resp => {
-                    const tagURL = resp.data['Post url'];
-                    tag['discourse_url'] = tagURL;
-                    dispatch(updateTagProp(tag, tagURL, 'discourse_url'));
-                    window.open(tag.discourse_url, '_blank');
-                  });
-                }
-              }}
-              disabled={!(isDiscourseAvailable() || (isDiscourseAvailableNotSignedIn() && tag.discourse_url))}
-            >
-              Discourse
-            </Button>
-          </Grid>
-        </Tooltip>
-      </Grid>
-      {/* user */}
-      <Grid item xs={1}>
-        <Typography variant="body2">{tag.user_id}</Typography>
-      </Grid>
-      {/* date */}
-      <Grid item xs={2} container alignContent="center" justifyContent="center">
+      <Tooltip title={CATEGORY_TYPE_BY_ID[tag.category_id]}>
         <Typography variant="body2" noWrap>
-          {navigator.language
-            ? new Date(tag.create_date).toLocaleDateString(navigator.language)
-            : new Date(tag.create_date).toLocaleDateString()}
+          {CATEGORY_TYPE_BY_ID[tag.category_id]}
         </Typography>
-      </Grid>
-      {/* edit button */}
-      <Grid item xs={1}>
-        <IconButton
-          variant="contained"
-          className={classes.editButton}
-          size="small"
-          onClick={() => handleEditTag(tag)}
-          disabled={!DJANGO_CONTEXT.pk}
-          aria-label="edit tag"
+      </Tooltip>
+      {/* select hits button */}
+      <Tooltip title="Select hits">
+        <Button
+          variant="outlined"
+          className={classNames(classes.contColButton, {
+            [classes.contColButtonSelected]: hasSelectedMolecule(),
+            [classes.contColButtonHalfSelected]: false
+          })}
+          onClick={() => handleSelectHits()}
+          disabled={false}
         >
-          <Tooltip title="Edit" className={classes.editButtonIcon}>
-            <Edit />
-          </Tooltip>
-        </IconButton>
-      </Grid>
-    </Grid>
+          {hasSelectedMolecule() ? 'Unselect hits' : 'Select hits'}
+        </Button>
+      </Tooltip>
+      {/* discourse button */}
+      <Tooltip title="Discourse link">
+        <Button
+          variant="outlined"
+          className={classNames(classes.contColButton, {
+            [classes.contColButtonSelected]: false,
+            [classes.contColButtonHalfSelected]: false
+          })}
+          onClick={() => {
+            if (isURL(tag.discourse_url)) {
+              window.open(tag.discourse_url, '_blank');
+            } else {
+              createTagPost(tag, targetName, getDefaultTagDiscoursePostText(tag)).then(resp => {
+                const tagURL = resp.data['Post url'];
+                tag['discourse_url'] = tagURL;
+                dispatch(updateTagProp(tag, tagURL, 'discourse_url'));
+                window.open(tag.discourse_url, '_blank');
+              });
+            }
+          }}
+          disabled={!(isDiscourseAvailable() || (isDiscourseAvailableNotSignedIn() && tag.discourse_url))}
+        >
+          Discourse
+        </Button>
+      </Tooltip>
+      {/* user */}
+      <Typography variant="body2">{tag.user_id}</Typography>
+      {/* date */}
+      <Typography variant="body2" noWrap>
+        {navigator.language
+          ? new Date(tag.create_date).toLocaleDateString(navigator.language)
+          : new Date(tag.create_date).toLocaleDateString()}
+      </Typography>
+      {/* edit button */}
+
+      <IconButton
+        variant="contained"
+        className={classes.editButton}
+        size="small"
+        onClick={() => handleEditTag(tag)}
+        disabled={!DJANGO_CONTEXT.pk}
+        aria-label="edit tag"
+      >
+        <Tooltip title="Edit" className={classes.editButtonIcon}>
+          <Edit />
+        </Tooltip>
+      </IconButton>
+    </div>
   );
 });
 
