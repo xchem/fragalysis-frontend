e5fb42cce1af5e564ef183ff51b8cbb8dc0c33d6
Boris Kovar <boris.kovar@m2ms.sk>
2022-07-18 13:23:12 +0200

    Squashed commit of the following:
    commit 949573bd930afaa0171a728f05dafa08c2425341
    Merge: deb0d1c3 74ea6613
    Author: Boris Kovar <boris.kovar@m2ms.sk>
    Date:   Thu Jul 14 06:56:52 2022 +0200
    
        Merge branch '#885-bugfix' of https://github.com/m2ms/fragalysis-frontend into #885-bugfix
    
    commit deb0d1c31aa68fb38c1d1541aca6bd81410e515b
    Author: Boris Kovar <boris.kovar@m2ms.sk>
    Date:   Thu Jun 9 10:21:43 2022 +0200
    
        - mics changes
    
    commit e2a00641039dd34b348ce2084eefd8f33ae04343
    Author: Boris Kovar <boris.kovar@m2ms.sk>
    Date:   Mon Jun 6 06:05:49 2022 +0200
    
        Squashed commit of the following:
    
        -bumped version to 0.10.93
    
        commit dfa24f68c4cb0e84ad37ac663ec08caadd2f3fbb
        Author: Anton Gasperak <anton.gasperak@m2ms.sk>
        Date:   Wed Jun 1 11:53:32 2022 +0200
    
            #883 - Remove unused selection action
    
        commit 0ae63081e822a514443a7b6c6e1fb3826a8c6178
        Author: Anton Gasperak <anton.gasperak@m2ms.sk>
        Date:   Wed Jun 1 11:43:17 2022 +0200
    
            #883 - Remove countOfPendingVectorLoadRequests from selectionReducers with unused hook
    
        commit b884e0b59c5e9000422adbf6e6adf7b14847b51f
        Author: Anton Gasperak <anton.gasperak@m2ms.sk>
        Date:   Wed Jun 1 11:10:44 2022 +0200
    
            #883 - Remove associatedDownloadTagName from selectionReducers
    
        commit ab825ccf3369af8fbaab9c8d25d68c5a1699d773
        Author: Anton Gasperak <anton.gasperak@m2ms.sk>
        Date:   Wed Jun 1 10:54:57 2022 +0200
    
            #883 - Remove displayedMoleculesInHitNav from selectionReducers
    
        commit 4103e3984bc9b3fbf2cc48bef3b9d65864a4814b
        Author: Anton Gasperak <anton.gasperak@m2ms.sk>
        Date:   Wed Jun 1 10:08:28 2022 +0200
    
            #883 - Remove displayAllInNGLList from selectionReducers
    
        commit f3b3b10a879447705b2709b88d48b62fe50e14cc
        Author: Anton Gasperak <anton.gasperak@m2ms.sk>
        Date:   Wed Jun 1 09:59:23 2022 +0200
    
            #883 - Remove listAllList from selectionReducers
    
        commit 02aa5955918abace2d5473e89bb9260ff10d7338
        Author: Anton Gasperak <anton.gasperak@m2ms.sk>
        Date:   Wed Jun 1 09:40:29 2022 +0200
    
            #883 - Move categoryList from selectionReducers to apiReducers
    
        commit d059ee8d4de0ddc34be64ef9c55a71c4f2bd96df
        Author: Anton Gasperak <anton.gasperak@m2ms.sk>
        Date:   Wed Jun 1 08:43:26 2022 +0200
    
            #883 - Move tagList from selectionReducers to apiReducers
    
    commit 1e6311eddebf515f11a42e86fe3a1db7572dcdba
    Author: Anton Gasperak <anton.gasperak@m2ms.sk>
    Date:   Mon Jul 11 14:42:22 2022 +0200
    
        #885 - Add the possibility to recompile variables before job launching
    
    commit 74ea6613da0306f5251a440bdfdaa44f225ccb69
    Author: Boris Kovar <boris.kovar@m2ms.sk>
    Date:   Thu Jun 9 10:21:43 2022 +0200
    
        - mics changes
    
    commit f98a7cac0c540cc9f9d52ec09814e86c80cee065
    Author: Anton Gasperak <anton.gasperak@m2ms.sk>
    Date:   Mon Jun 6 12:28:04 2022 +0200
    
        #885 - Adjust and compile schema
    
    commit d2e422992f95edb2a82435f03d8793f8c3e72bc1
    Author: Anton Gasperak <anton.gasperak@m2ms.sk>
    Date:   Fri Jun 3 12:53:17 2022 +0200
    
        #885 - Reuse the merged schema in JobVariablesDialog
    
    commit bd3ff2b3f22dada64afadd4df1685bcc46ab2d21
    Author: Anton Gasperak <anton.gasperak@m2ms.sk>
    Date:   Fri Jun 3 08:36:34 2022 +0200
    
        #885 - Add new job definition

--- js/components/preview/projectHistoryPanel/JobConfigurationDialog.js
+++ js/components/preview/projectHistoryPanel/JobConfigurationDialog.js
@@ -194,14 +194,6 @@ const JobConfigurationDialog = ({ snapshots }) => {
     return newTitle;
   };
 
-  const getFragmentTemplate = fragment => {
-    return `fragalysis-files/${target_on_name}/${fragment}.mol`;
-  };
-
-  const getProteinTemplate = protein => {
-    return `fragalysis-files/${target_on_name}/${protein}_apo-desolv.pdb`;
-  };
-
   const getMoleculeTitle = title => {
     return title.replace(new RegExp(':.*$', 'i'), '');
   };
@@ -364,14 +356,7 @@ const JobConfigurationDialog = ({ snapshots }) => {
           snapshot: chosenSnapshot,
           // Prepares data for expanding, see comments in JobFragmentProteinSelectWindow
           data: {
-            'lhs-fragments': {
-              enum: chosenLHSCompounds.map(fragment => getFragmentTemplate(fragment)),
-              enumNames: chosenLHSCompounds.map(compound => getMoleculeEnumName(compound))
-            },
-            'lhs-protein-apo-desolv': {
-              enum: chosenLHSCompounds.map(protein => getProteinTemplate(protein)),
-              enumNames: chosenLHSCompounds.map(compound => getMoleculeEnumName(compound))
-            }
+            lhs: chosenLHSCompounds.map(compound => getMoleculeEnumName(compound))
           }
         })
       );
@@ -380,6 +365,7 @@ const JobConfigurationDialog = ({ snapshots }) => {
         snapshot: chosenSnapshot.id,
         target: targetId,
         // squonk_project: dispatch(getSquonkProject()),
+        // need to switch to squonk project id associated with the target in the (near?) future
         squonk_project: 'project-e1ce441e-c4d1-4ad1-9057-1a11dbdccebe',
         proteins: chosenLHSCompounds.join(),
         compounds: chosenRHSCompounds.join()
