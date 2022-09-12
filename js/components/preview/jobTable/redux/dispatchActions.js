a08e67b55ecce618013620d0f64c07ce34c25136
Boris Kovar <boris.kovar@m2ms.sk>
2022-09-12 11:47:57 +0200

    - fixed tab switching

--- js/components/preview/jobTable/redux/dispatchActions.js
+++ js/components/preview/jobTable/redux/dispatchActions.js
@@ -1,21 +1,23 @@
-import { setSelectedDatasetIndex } from '../../../datasets/redux/actions';
+import { setSelectedDatasetIndex, setTabValue } from '../../../datasets/redux/actions';
 import { turnSide } from '../../viewerControls/redux/actions';
 
 export const selectDatasetResultsForJob = jobInfo => (dispatch, getState) => {
   const state = getState();
   const datasetsList = state.datasetsReducers.datasets;
   const currentDatasetIndex = state.datasetsReducers.selectedDatasetIndex;
+  const tabValue = state.datasetsReducers.tabValue;
   let datasetIndex = datasetsList.findIndex(dataset => dataset.id === jobInfo.computed_set);
   console.log(`Found dataset index ${datasetIndex} for job ${JSON.stringify(jobInfo)}`);
   if (datasetIndex >= 0) {
     dispatch(turnSide('RHS', true));
     const oldTitle = datasetsList[currentDatasetIndex]?.title;
     const newTitle = datasetsList[datasetIndex]?.title;
-    datasetIndex = datasetIndex + 2;
+    // datasetIndex = datasetIndex + 2;
     console.log(`Selecting dataset index ${datasetIndex}`);
     console.log(
       `from job table - currentDatasetIndex: ${currentDatasetIndex} datasetIndex: ${datasetIndex} newTitle: ${newTitle} oldTitle: ${oldTitle}`
     );
     dispatch(setSelectedDatasetIndex(currentDatasetIndex, datasetIndex, newTitle, oldTitle));
+    dispatch(setTabValue(tabValue, 2, oldTitle, newTitle));
   }
 };
