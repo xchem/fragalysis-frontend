cc2994cc98d57bed517fb671e56fb17c006e2d04
Boris Kovar <boris.kovar@m2ms.sk>
2022-09-12 10:42:25 +0200

    - fixes for selecting dataset

--- js/components/preview/jobTable/redux/dispatchActions.js
+++ js/components/preview/jobTable/redux/dispatchActions.js
@@ -4,12 +4,15 @@ import { turnSide } from '../../viewerControls/redux/actions';
 export const selectDatasetResultsForJob = jobInfo => (dispatch, getState) => {
   const state = getState();
   const datasetsList = state.datasetsReducers.datasets;
+  const currentDatasetIndex = state.datasetsReducers.selectedDatasetIndex;
   let datasetIndex = datasetsList.findIndex(dataset => dataset.id === jobInfo.computed_set);
   console.log(`Found dataset index ${datasetIndex} for job ${JSON.stringify(jobInfo)}`);
   if (datasetIndex >= 0) {
     dispatch(turnSide('RHS', true));
+    const oldTitle = datasetsList[currentDatasetIndex]?.title;
+    const newTitle = datasetsList[datasetIndex]?.title;
     datasetIndex = datasetIndex + 2;
     console.log(`Selecting dataset index ${datasetIndex}`);
-    dispatch(setSelectedDatasetIndex(datasetIndex));
+    dispatch(setSelectedDatasetIndex(currentDatasetIndex, datasetIndex, newTitle, oldTitle));
   }
 };
