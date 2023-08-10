39f2f2560af0a94e69b5dd5c0622c41bf944c2c7
boriskovar-m2ms <boris.kovar@m2ms.sk>
2023-08-10 12:49:41 +0200

    #944 - get job definitions from backend (#363)
    * - initial commit
    
    * - implemented all the features for #944

--- js/components/preview/withLoadingJobSpecs.js
+++ js/components/preview/withLoadingJobSpecs.js
@@ -1,14 +1,22 @@
 import React, { memo, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { getJobConfigurations } from '../projects/redux/dispatchActions';
+import { getJobConfigurationsFromServer } from '../projects/redux/dispatchActions';
+import { DJANGO_CONTEXT } from '../../utils/djangoContext';
+import { setJobList } from '../projects/redux/actions';
 
 export const withLoadingJobSpecs = WrappedComponent => {
   return memo(({ ...rest }) => {
     const dispatch = useDispatch();
 
-    useEffect(() => {
-      dispatch(getJobConfigurations());
-    }, [dispatch]);
+    const authenticated = DJANGO_CONTEXT['authenticated'];
+
+    //now only authenticated users can see the job specs
+    // useEffect(() => {
+    //   if (authenticated) {
+    //     const jobsList = dispatch(getJobConfigurationsFromServer());
+    //     dispatch(setJobList(jobsList));
+    //   }
+    // }, [dispatch, authenticated]);
 
     return <WrappedComponent {...rest} />;
   });
