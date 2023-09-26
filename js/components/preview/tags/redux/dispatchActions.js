d78122415ff1d8ad8982611e8338f44231181f2c
Boris Kovar <boris.kovar@m2ms.sk>
2023-09-26 09:06:29 +0200

    - initial implementation of #1139 - tags and molecules

--- js/components/preview/tags/redux/dispatchActions.js
+++ js/components/preview/tags/redux/dispatchActions.js
@@ -242,7 +242,7 @@ export const loadMoleculesAndTagsNew = targetId => async (dispatch, getState) =>
       let newObject = { ...mol };
       const tagsForMol = getTagsForMol(mol.id, tags);
       if (tagsForMol) {
-        newObject['tags_set'] = [...tagsForMol.map(t => t.id)];
+        newObject['tags_set'] = [...tagsForMol];
       } else {
         newObject['tags_set'] = [];
       }
@@ -250,10 +250,10 @@ export const loadMoleculesAndTagsNew = targetId => async (dispatch, getState) =>
     });
 
     allMolecules?.sort((a, b) => {
-      if (a.code < b.code) {
+      if (a.protein_code < b.protein_code) {
         return -1;
       }
-      if (a.code > b.code) {
+      if (a.protein_code > b.protein_code) {
         return 1;
       }
       return 0;
