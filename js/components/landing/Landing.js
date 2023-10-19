a0595fecaa824a9d114facfaed30d1ae2f19daf6
matej <matej.vavrek@m2ms.sk>
2023-10-19 09:16:56 +0200

    do not set logged in toast message if user is not logged in

--- js/components/landing/Landing.js
+++ js/components/landing/Landing.js
@@ -28,7 +28,7 @@ const Landing = memo(
     const classes = useStyles();
 
     const { toast } = useContext(ToastContext);
-    const [loginText, setLoginText] = useState("You're logged in as " + DJANGO_CONTEXT['username']);
+    const [loginText, setLoginText] = useState(DJANGO_CONTEXT['username'] === 'NOT_LOGGED_IN' ? '' : "You're logged in as " + DJANGO_CONTEXT['username']);
 
     useEffect(() => {
       if (DJANGO_CONTEXT['authenticated'] !== true) {
