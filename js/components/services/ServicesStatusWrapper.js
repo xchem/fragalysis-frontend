45100f21e9d4740019592e4c1b19f24fc584ac64
matej <matej.vavrek@m2ms.sk>
2024-01-27 18:28:34 +0100

    #1208 added check for status change

--- js/components/services/ServicesStatusWrapper.js
+++ js/components/services/ServicesStatusWrapper.js
@@ -1,19 +1,38 @@
 import { Grid } from "@material-ui/core";
-import React, { memo, useEffect, useState } from "react";
+import React, { memo, useCallback, useContext, useEffect, useState } from "react";
 import { ServicesStatus } from "./ServicesStatus";
 import { getServicesStatus } from "./api/api";
+import { ToastContext } from "../toast";
 
 export const ServicesStatusWrapper = memo(() => {
     const [services, setServices] = useState([]);
+    const { toastError, toastInfo } = useContext(ToastContext);
 
-    const fetchServicesStatus = async () => {
+    const checkServices = useCallback((previous, current) => {
+        if (previous.length > 0) {
+            const changedServices = current.filter(newService => {
+                const currentService = previous.find(previousService => previousService.id === newService.id);
+                if (currentService && currentService.state !== newService.state) {
+                    return true;
+                }
+                return false;
+            });
+            changedServices.forEach(service => toastInfo(`Status of ${service.name} changed to ${service.state}`));
+        }
+    }, [toastInfo]);
+
+    const fetchServicesStatus = useCallback(async () => {
         const temp = await getServicesStatus();
         if (!!!temp || temp.length === 0) {
+            toastError('Status of services is not available');
             setServices([{ id: 'services', name: 'Status of services', state: 'NOT_AVAILABLE' }]);
         } else {
-            setServices(temp);
+            setServices((prevState) => {
+                checkServices(prevState, temp);
+                return temp;
+            });
         }
-    }
+    }, [checkServices, toastError]);
 
     useEffect(() => {
         fetchServicesStatus();
@@ -22,7 +41,7 @@ export const ServicesStatusWrapper = memo(() => {
         return () => {
             clearInterval(interval);
         }
-    }, []);
+    }, [fetchServicesStatus]);
 
     return <Grid item>
         <ServicesStatus services={services} />
