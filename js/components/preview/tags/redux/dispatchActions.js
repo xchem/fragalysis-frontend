7aa231a630103579c1170efd7a65016ff12c09f8
Boris Kovar <boris.kovar@m2ms.sk>
2026-07-15 10:54:03 +0200

    - fixed orphaned observation

--- js/components/preview/tags/redux/dispatchActions.js
+++ js/components/preview/tags/redux/dispatchActions.js
@@ -273,11 +273,7 @@ export const loadMoleculesAndTagsNew = targetId => async (dispatch, getState) =>
       const computedInspirationId = Number(item?.computed_inspiration);
       const computedSetId = Number(item?.computed_set);
 
-      if (
-        Number.isNaN(siteObservationId) ||
-        Number.isNaN(computedInspirationId) ||
-        Number.isNaN(computedSetId)
-      ) {
+      if (Number.isNaN(siteObservationId) || Number.isNaN(computedInspirationId) || Number.isNaN(computedSetId)) {
         return;
       }
 
@@ -403,7 +399,7 @@ export const loadMoleculesAndTagsNew = targetId => async (dispatch, getState) =>
         newObject['associatedObs'] = associatedObs;
 
         // rhs poses are not linked to an experiment - currently this can't happen because there are no poses for rhs virtual observations
-        if (firstObs.experiment === null) {
+        if (firstObs?.experiment === null) {
           modifiedRhsPoses.push(newObject);
         } else {
           modifiedLhsPoses.push(newObject);
